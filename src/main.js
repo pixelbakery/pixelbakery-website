4a3df360f07f345035ef9248858d8c47888abfab
Jordan Lambrecht <jordan@pixelbakery.co>
2021-10-09 01:45:55 -0500

    added client list section

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import "./css/style.css";
 import "./css/typefaces.css";
+
+
+
+import "./components/GSAPclients.js";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,40 @@ const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./src//index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'xclass'
   theme: {
     extend: {
-      colors:{
-        teal: colors.teal,
-        cyan: colors.cyan,
-        orange: colors.orange,
-        tan: "#D9C2b3",
+      fontFamily: {
+        bigjohnpro: "Big John Pro, sans-serif",
+        pbheading: "Gilroy, Poppins, sans-serif",
+        body: "Poppins, sans-serif",
       },
-      backgroundImage: {
-        // +         'footer-texture': "url('/img/footer-texture.png')",
-                }
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        },
+        pb: {
+          blue: {
+            DEFAULT: "rgba(110, 195, 220, 1)",    //#6EC3DC
+            dark: "rgba(4, 81, 113, 1)",          //#045171
+          },
+          peach: "rgba(237, 105, 112, 1)",        //#ED6969
+          yellow: "rgba(245, 215, 122, 1)",       //#F5D77A
+          pink: "rgba(242, 168, 202, 1)",         //#F2A8CA
+          wine: "rgba(80, 78, 87, 1)",            //#504E57
+          whipped: "rgba(239, 232, 242, 1) ",     //#EFE8F2
+        },
+        
+      },
+      
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"]
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
