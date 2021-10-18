a4ffb20cc732c669785699b82186c29d76ecbed9
Jordan Lambrecht <jordan@pixelbakery.co>
2021-10-18 15:44:41 -0500

    merging mult. local versions

--- src/components/work-portfolio.jsx
+++ src/components/work-portfolio.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import PortfolioCard from "../components/work-portfolio-card";
-import projects from "../data/portfolio";
+import projects from "../data/portfolio"
+
 
 export default function Portfolio(){
+  return(
+    <section className="" id="work-portfolio">
 
-    return(
-        <section className="" id="work-portfolio">
+      <div id="portfolioFilter">
+          <button data-filter="*">All</button>
+          <button data-filter=".animation">Animation & Motion Design</button>
+          <button data-filter=".film">Commercial Film Production</button>
+          <button data-filter=".concepting">Creative Concepting & Strategy</button>
+          <button data-filter=".3dmodeling">3D Modeling</button>
+          <button data-filter=".emergingmedia">Emerging Media</button>
+          <button data-filter=".social">Social Media Kitouts</button>
+      </div>
+      <div className="portfolio-isotopes grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 "> 
+          { projects.map(project=>(
+              <PortfolioCard project={project} key={project.name} />
+          ))}
+      </div>
 
 
-            <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 "> 
-                { projects.map(project=>(
-                    <PortfolioCard project={project} key={project.project} />
-                ))}
-            </div>
-        </section>
-    )
+  </section>        
+  )
 }
+
+
+
+
