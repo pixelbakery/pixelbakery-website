f4dcb3fd6436b92f389851dfc715e0adaac2ecd2
Jordan Lambrecht <jordan@pixelbakery.co>
2021-10-19 12:36:59 -0500

    Added dynamic routes for team bio pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,16 @@
 import React from "react";
+import roster from "../data/team";
 
 function About() {
   return (
-    <div className="about">
-      <div className="container">
-        <div className="row align-items-center my-5">
-          <div className="col-lg-7">
-            <img
-              className="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+    <section className="" id="about">
+      {roster.map((value, index) => {        
+        return <div className="h-64 w-64 m-8">
+             <a key={index} href={"/"+value["url"]}><img className="w-full h-full object-cover" src={value["image_headshot"]} /></a>
           </div>
-          <div className="col-lg-5">
-            <h1 className="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+         
+      })}
+    </section>
   );
 }
 
