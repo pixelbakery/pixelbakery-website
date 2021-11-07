431c4847cefaec31fa50a54be29626667a29b5f4
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-07 13:16:09 -0600

    💅 styling

--- src/components/pg-home/homeLanding.jsx
+++ src/components/pg-home/homeLanding.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import {Link, NavLink} from 'react-router-dom';
 import SocialLinks from "../parts/socialOptions";
 import ButtonFilled from "../parts/button-filled";
-import MainLogo from "../parts/main-logo";
+// import MainLogo from "../parts/main-logo";
 
 export default function homeLanding(){
     return(
         <div>
-            <section className="lander w-full my-4 flex">
+            <section className="lander overflow-hidden w-full my-4 flex">
                 <nav className="w-1/3 max-w-lg bg-pink-light h-full flex flex-col justify-between py-10 px-12 ">
                     
                        
                             <div className="logo-wrapper px-20 w-full text-blue fill-current">
-                                <MainLogo color={"fill-pink"}/>
-                                {/* <img 
-                                    className=" object-center block m-auto object-contain w-full text-blue fill-current"
+                                <img 
+                                    className=" object-center block m-auto object-contain w-full logo-pink"
                                     src="./src/img/pixelbakery-logo-primary.svg" 
+                                    
                                     id="home-logo"
                                     alt="pixel bakery is an animation and video production studio" 
-                                /> */}
+                                />
                             </div>
                     <div className="mx-auto w-full px-4">
                         <div className="mx-auto mt-12" id="homepage-main-nav">
                             <ul className="">
-                                <li className="my-2"><NavLink to={"/work"} className="text-pink font-bold lowercase text-4xl">work</NavLink></li>
-                                <li className="my-2"><NavLink to={"/about"} className="text-pink font-bold lowercase text-4xl">bakery</NavLink></li>
-                                <li className="my-2"><NavLink to={"/education"} className="text-pink font-bold lowercase text-4xl">education</NavLink></li>
-                                <li className="my-2"><NavLink to={"/recipesss"} className="text-pink font-bold lowercase text-4xl">mom's recipes</NavLink></li>
+                                <li className="my-1"><NavLink to={"/work"} className="text-pink font-bold lowercase text-4xl">work</NavLink></li>
+                                <li className="my-1"><NavLink to={"/about"} className="text-pink font-bold lowercase text-4xl">bakery</NavLink></li>
+                                <li className="my-1"><NavLink to={"/education"} className="text-pink font-bold lowercase text-4xl">education</NavLink></li>
+                                <li className="my-1"><NavLink to={"/recipesss"} className="text-pink font-bold lowercase text-4xl">mom's recipes</NavLink></li>
                                 <li className="my-2"><NavLink to={"/store"} className="text-pink font-bold lowercase text-4xl">company store</NavLink></li>
                             </ul>
                         </div>
