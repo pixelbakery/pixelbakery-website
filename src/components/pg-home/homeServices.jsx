f2dce93d65d4a00c35b9d2fa022b407b8c511008
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-07 10:22:15 -0600

    🚀 home page improvementss

--- src/components/pg-home/homeServices.jsx
+++ src/components/pg-home/homeServices.jsx
@@ -5,8 +5,8 @@ import ServiceCard from "./home-services-card";
 
 export default function Services(){
     return(
-        <section className="py-16 bg-blue " id="services">
-                <div className="max-w-6xl mx-auto">
+        <section className="relative py-16 bg-blue overflow-hidden" id="home-services">
+                <div className="relative max-w-6xl mx-auto z-10">
                     <div className=" w-full flex flex-col ">
                         <div className="w-1/2 mb-16">
                             <h2 className="text-blue-dark">And we're the best at what we do. </h2>
@@ -20,6 +20,14 @@ export default function Services(){
                         
                         
                     </div>
+                </div>
+                <div className="absolute w-full max-w-xl z-0" id="home-sifter">
+                    <img
+                        src="./src/img/icons/abs/pixelbakery_icon_sifter_abs.svg"
+                        id=""
+                        className=""
+
+                    />
                 </div>
         </section>
 
