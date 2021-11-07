642cb86cdf0ffcf7ecbb565b27c4816ab7515324
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-07 10:29:32 -0600

    🌈 updated theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,20 @@ module.exports = {
       white: "#ffffff",
       egg: "#F3F4F6",
       blue: {
-        light: "rgba(204, 238, 255,1)",
+        light: "rgb(204, 238, 255)",
         // DEFAULT: "rgba(102, 204, 255, 1)",
-        DEFAULT: "rgba(56, 204, 255, 1)",
-        dark: "rgba(0, 81, 116, 1)",
+        DEFAULT: "rgb(56, 204, 255)",
+        dark: "rgb(0, 81, 116)",
       },
       // peach: "rgba(237, 105, 112, 1)",        //#ED6969
-      peach: "rgba(255, 94, 100, 1)", 
-      yellow: "rgba(250, 216, 105, 1)",       //#F5D77A
+      peach: "rgb(255, 94, 100)", 
+      yellow: "rgb(250, 216, 105)",       //#F5D77A
       pink:{ 
-        light: "RGBA(241, 231, 243,1)",					 
-        DEFAULT: "rgba(255, 163, 204, 1)",         //#F2A8CA
+        light: "rgb(241, 231, 243)",					 
+        DEFAULT: "rgb(255, 163, 204)",         //#F2A8CA
       },
-      wine: "rgba(80, 78, 87, 1)",            //#504E57
-      cream: "rgba(243, 244, 246, 1) ",     //#EFE8F2
+      wine: "rgb(80, 78, 87)",            //#504E57
+      cream: "rgb(243, 244, 246) ",     //#EFE8F2
     },
     fontFamily: {
       pbheading: "Gilroy, Poppins, sans-serif",
