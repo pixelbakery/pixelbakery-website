e09fc89614557fe9dbc153dddc3647b8c75ba2f9
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-07 13:18:45 -0600

    🏗  began js animation on FAQ section, code cleanu

--- src/components/pg-about/about-faq-card.jsx
+++ src/components/pg-about/about-faq-card.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
 export default function FAQcard({faq}){
+    
     return(
-        <div>
-             <div className="border-2 rounded-md border-blue px-4 py-3 my-4">
-            <p className="text-pink font-bold py-1 my-0 text-lg">{faq.q}</p>
+        <div className="faqcard">
+             <button className="block w-full border-2 rounded-md border-blue px-4 py-3 my-4 question">
+                <p className="text-pink font-bold py-1 my-0 text-lg">{faq.q}</p>
+
+
+                <div className="w-4 h-4 circle-plus closed">
+                    <div className=" circle">
+                        <div className="absolute horizontal bg-blue"></div>
+                        <div className="absolute vertical bg-blue"></div>
+                    </div>
+                </div>
+            
+
+
+
+            </button>
+            <div className="answer">
+                <p className="mx-6  text-blue-dark ">
+                    {faq.a}
+                </p>
             </div>
-            <p className="mx-6  text-blue-dark ">
-                {faq.a}
-            </p>
         </div>
        
             
--- src/components/pg-about/about-faq.jsx
+++ src/components/pg-about/about-faq.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import faq from "../../data/faq";
 import FAQbox from "./about-faq-card";
 
 export default function FAQ(){
+    useEffect(() => {
+        const faqcard = document.querySelectorAll(".faqcard");
+        faqcard.forEach(faq => {
+            let buttonEl = faq.firstChild;
+            let answerEl = faq.lastChild;
+            
+            buttonEl.onclick = function() {
+                
+                buttonEl.classList.toggle("opened");
+                answerEl.classList.toggle("opened");
+            }
+        });
+        return () => {
+          
+        }
+      }, [])
     return(
-        <section className="py-8 my-4 bg-cream">
+        <section className="py-8 my-4 bg-pink-light">
             <div className="wrapper mx-auto max-w-5xl">
             <h2 className="text-blue max-w-lg"> Some questions we get asked frequently</h2>
         { faq.map(faq=>(
