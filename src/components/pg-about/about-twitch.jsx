e9ff19b5c06879365f1c71ffcb9c52f91503604a
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-14 17:43:41 -0600

    added Twitch stream to about page

--- src/components/pg-about/about-twitch.jsx
+++ src/components/pg-about/about-twitch.jsx
@@ -1,21 +1,61 @@
-import React from "react";
-import "https://embed.twitch.tv/embed/v1.js";
-
-new Twitch.Embed("twitch-embed", {
-    width: 854,
-    height: 480,
-    channel: "monstercat",
-    // Only needed if this page is going to be embedded on other websites
-    parent: ["embed.example.com", "localhost:3000"]
-  });
+import React, { useEffect } from "react";
+import ReactPlayer from 'react-player/twitch'
 
 
-export default function Twitch(){
-    return(
-        <section className="">
-      
-        <div id="twitch-embed"></div>
-    </section>
-    );
+// const helmet = <Helmet><script type="module"  src="https://embed.twitch.tv/embed/v1.js" /></Helmet>
+
+
+
+function TwitchStream(){
     
-}
+
+    // useEffect(() => {
+    //     // <script type="text/javascript">
+    //     const script = document.createElement('script');
+    // script.src = "https://embed.twitch.tv/embed/v1.js";
+    // script.async = true;
+    // document.body.appendChild(script);
+    //     new Twitch.Embed("twitch-embed", {
+    //       width: 854,
+    //       height: 480,
+    //       channel: "monstercat",
+    //       // Only needed if this page is going to be embedded on other websites
+    //       parent: ["embed.example.com", "othersite.example.com"]
+    //     });
+    // //   </script>
+    //   return () => {
+    //       document.body.removeChild(script);
+    //     }
+    //   }, []);
+
+    
+    return (
+        <section className="relative mt-48 mb-4 h-full">
+
+            <div className="relative z-10 max-w-8xl">
+
+
+                <div id="twitch-title" className="z-20 absolute bg-pink-light px-20 max-w-3xl place-self-center">
+                    <h2 className="text-7xl text-peach -mt-12">Peep on us</h2>
+                    <p className="pb-10 text-pink leading-relaxed font-semibold text-lg">Working from home? Pretend you're not and vibe with us. Can't sleep? Cozy up with us in your bed and listen to the ryhtemic sounds of computers clicking. <a className="text-blue underline" href="twitch.tv/pixelbakery" target="_blank">Follow us on twitch</a> because we're superficial and want to get affiliate status.</p>
+                </div>
+
+
+                <div className="aspect-w-9 md:aspect-w-16 md:aspect-h-9 ">
+                        
+                    <ReactPlayer
+                            url="https://www.twitch.tv/pixelbakery"
+                            controls
+                            autoplay={true}
+                            muted={true}
+                            width={"100%"}
+                            height={"100%"}
+                    />
+                </div>
+            </div>
+        </section>
+
+    
+    ); 
+} 
+export default TwitchStream;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import roster from "../data/team";
 import FAQ from "../components/pg-about/about-faq";
+import Twitch from "../components/pg-about/about-twitch";
 // import TwitchStream from "../components/about-twitch";
 function About() {
   return (
-    <>
+    <main>
     <section className="" id="about">
       {roster.map((value, index) => {        
         return <div className="h-64 w-64 m-8">
@@ -13,8 +14,10 @@ function About() {
          
       })}
     </section>
+    <section className="h-screen bg-blue"></section>
+    <Twitch />
     <FAQ/>
-    </>
+    </main>
 
   );
 }
