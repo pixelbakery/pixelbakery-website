d58a60644d00a87ac42cd179852f25e8fd338617
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-13 20:18:42 -0600

    ✨ cleaned icon structure

---
+++ src/components/parts/icons.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import icons from '../../data/pbIcons'
+
+
+
+
+function PBicons(){
+     
+     
+     return(
+          <div>
+          test
+       
+           
+          </div>
+     );
+}
+export default PBicons
