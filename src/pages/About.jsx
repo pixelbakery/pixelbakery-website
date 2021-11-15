43946e9d4f6bef26751a69dcd58ad66bdf3f1cfb
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-15 01:15:44 -0600

    👩‍🍳 Added additional team members

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,13 +9,18 @@ import FAQ from "../components/pg-about/about-faq";
 function About() {
   return (
     <main>
-    <section className="" id="about">
+      <section className="lander my-4 bg-blue">
+        lander
+      </section>
+    <section className="my-4" id="about">
+      <div class="grid gap-4 grid-cols-4">
       {roster.map((value, index) => {        
-        return <div className="h-64 w-64 m-8">
+        return <div className="w-full aspect-w-3 aspect-h-4">
              <a key={index} href={"/"+value["url"]}><img className="w-full h-full object-cover" src={value["image_headshot"]} /></a>
           </div>
          
       })}
+      </div>
     </section>
     <section className="h-screen bg-blue"></section>
     <StudioViews />
