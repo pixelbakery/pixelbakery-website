ac958706c4c6489c65bb943a82de869b65f3c473
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-21 12:15:06 -0600

    📦 moved sections to their own components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import roster from "../data/team";
+
 
 import StudioViews from "../components/pg-about/about-studioViews";
+import Team from "../components/pg-about/about-team";
+import Values from "../components/pg-about/about-values";
 import Twitch from "../components/pg-about/about-twitch";
 import FAQ from "../components/pg-about/about-faq";
+
 
 // import TwitchStream from "../components/about-twitch";
 function About() {
@@ -12,17 +15,8 @@ function About() {
       <section className="lander my-4 bg-blue">
         lander
       </section>
-    <section className="my-4" id="about">
-      <div class="grid gap-4 grid-cols-4">
-      {roster.map((value, index) => {        
-        return <div className="w-full aspect-w-3 aspect-h-4">
-             <a key={index} href={"/"+value["url"]}><img className="w-full h-full object-cover" src={value["image_headshot"]} /></a>
-          </div>
-         
-      })}
-      </div>
-    </section>
-    <section className="h-screen bg-blue"></section>
+    <Team/>
+    <Values />
     <StudioViews />
     <Twitch />
     <FAQ/>
