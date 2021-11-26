4525e251e4d0ada55a0a80b14473025a553332b5
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-26 06:26:15 -0600

    🏪 ADDED main store page for all productss

--- src/pages/Sitemap.jsx
+++ src/pages/Sitemap.jsx
@@ -2,10 +2,33 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import SitemapTable from "../components/parts/sitemap-table";
 
-function ComponentName() {
+function SiteMap() {
   return (
     <main id='' className='my-4 py-24 bg-pink-light'>
       <div className='mx-auto pt-14 max-w-5xl'>
+        <div className='my-16'>
+          <h1 className='text-6xl text-wine-200'>Quick Links</h1>
+          <ul className='text-wine text-lg mt-8 py-3 pr-2 list-disc list-inside'>
+            <li>
+              <NavLink
+                to='https://github.com/jordanlambrecht/pb-oct-2022'
+                className='underline'
+                target='_blank'
+              >
+                GitHub
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='https://pixelbakery.monday.com/boards/911081678/'
+                className='underline'
+                target='_blank'
+              >
+                Monday Board
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <h1 className='text-6xl text-wine-200'>Sitemap</h1>
       </div>
       <SitemapTable category={"primary"} />
@@ -15,4 +38,4 @@ function ComponentName() {
   );
 }
 
-export default ComponentName;
+export default SiteMap;
