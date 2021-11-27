3f670f57c791c22521f5bd0424eeec3cc2abadf0
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-27 00:27:43 -0600

    commercejs experimentation

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,18 +1,63 @@
 import React, { Component } from "react";
 import { commerce } from "../lib/Commerce";
-import ProductsList from "../components/ProductsList";
 
-class ProductFeed extends Component {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingBag, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Redirect, BrowserRouter, Switch, Route } from "react-router-dom";
+import ProductsList from "../components/ProductsList";
+import Cart from "../components/Cart";
+import Checkout from "./store/Checkout";
+
+library.add(faShoppingBag, faTimes);
+
+class Store extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      merchant: {},
       products: [],
+      cart: {},
+      isCartVisible: false,
     };
+
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.handleUpdateCartQty = this.handleUpdateCartQty.bind(this);
+    this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
+    this.handleEmptyCart = this.handleEmptyCart.bind(this);
+    this.toggleCart = this.toggleCart.bind(this);
   }
 
   componentDidMount() {
+    this.fetchMerchantDetails();
     this.fetchProducts();
+    this.fetchCart();
+  }
+
+  /**
+   * Show hide cart in nav
+   */
+  toggleCart() {
+    const { isCartVisible } = this.state;
+    this.setState({
+      isCartVisible: !isCartVisible,
+    });
+  }
+
+  /**
+   * Fetch merchant details
+   * https://commercejs.com/docs/sdk/full-sdk-reference#merchants
+   */
+  fetchMerchantDetails() {
+    commerce.merchants
+      .about()
+      .then((merchant) => {
+        this.setState({ merchant: merchant });
+      })
+      .catch((error) => {
+        console.log("There was an error fetch the merchant details", error);
+      });
   }
 
   /**
@@ -30,20 +75,181 @@ class ProductFeed extends Component {
       });
   }
 
-  render() {
-    const { products } = this.state;
+  /**
+   * Retrieve the current cart or create one if one does not exist
+   * https://commercejs.com/docs/sdk/cart
+   */
+  fetchCart() {
+    commerce.cart
+      .retrieve()
+      .then((cart) => {
+        this.setState({ cart: cart });
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the cart", error);
+      });
+  }
 
-    return <ProductsList products={products} />;
+  /**
+   * Adds a product to the current cart in session
+   * https://commercejs.com/docs/sdk/cart/#add-to-cart
+   *
+   * @param {string} productId The ID of the product being added
+   * @param {number} quantity The quantity of the product being added
+   */
+  handleAddToCart(productId, quantity) {
+    commerce.cart
+      .add(productId, quantity)
+      .then((item) => {
+        this.setState({ cart: item.cart });
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+      });
+  }
+
+  /**
+   * Updates line_items in cart
+   * https://commercejs.com/docs/sdk/cart/#update-cart
+   *
+   * @param {string} lineItemId ID of the cart line item being updated
+   * @param {number} newQuantity New line item quantity to update
+   */
+  handleUpdateCartQty(lineItemId, quantity) {
+    commerce.cart
+      .update(lineItemId, { quantity })
+      .then((resp) => {
+        this.setState({ cart: resp.cart });
+      })
+      .catch((error) => {
+        console.log("There was an error updating the cart items", error);
+      });
+  }
+
+  /**
+   * Removes line item from cart
+   * https://commercejs.com/docs/sdk/cart/#remove-from-cart
+   *
+   * @param {string} lineItemId ID of the line item being removed
+   */
+  handleRemoveFromCart(lineItemId) {
+    commerce.cart
+      .remove(lineItemId)
+      .then((resp) => {
+        this.setState({
+          cart: resp.cart,
+        });
+      })
+      .catch((error) => {
+        console.error(
+          "There was an error removing the item from the cart",
+          error
+        );
+      });
+  }
+
+  /**
+   * Empties cart contents
+   * https://commercejs.com/docs/sdk/cart/#remove-from-cart
+   */
+  handleEmptyCart() {
+    commerce.cart
+      .empty()
+      .then((resp) => {
+        this.setState({ cart: resp.cart });
+      })
+      .catch((error) => {
+        console.error("There was an error emptying the cart", error);
+      });
+  }
+
+  renderCartNav() {
+    const { cart, isCartVisible } = this.state;
+
+    return (
+      <div className='nav'>
+        <div className='nav__cart' onClick={this.toggleCart}>
+          {!isCartVisible ? (
+            <button className='nav__cart-open'>
+              <FontAwesomeIcon size='2x' icon='shopping-bag' color='#292B83' />
+              {cart !== null ? <span>{cart.total_items}</span> : ""}
+            </button>
+          ) : (
+            <button className='nav__cart-close'>
+              <FontAwesomeIcon size='1x' icon='times' color='white' />
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { products, merchant, cart, isCartVisible } = this.state;
+
+    return (
+      <main>
+        <section id='store' className='my-4 bg-blue-dark py-40'>
+          <div className='mx-auto max-w-6xl'>
+            <h1 className='text-pink mb-20 '>Company Shop</h1>
+            {this.renderCartNav()}
+            {isCartVisible && (
+              <Cart
+                cart={cart}
+                onUpdateCartQty={this.handleUpdateCartQty}
+                onRemoveFromCart={this.handleRemoveFromCart}
+                onEmptyCart={this.handleEmptyCart}
+              />
+            )}
+
+            <ProductsList
+              products={products}
+              onAddToCart={this.handleAddToCart}
+            />
+          </div>
+        </section>
+        <Switch>
+          <Route
+            path='/store/checkout'
+            exact
+            render={(props) => {
+              return (
+                <Checkout
+                  {...props}
+                  cart={cart}
+                  onCaptureCheckout={this.handleCaptureCheckout}
+                />
+              );
+            }}
+          />
+        </Switch>
+      </main>
+    );
   }
 }
 
-export default ProductFeed;
+export default Store;
+
+//   render() {
+//
+
+//     return (
+//
+//
+//
+//
+//
+
+//
+//           </div>
+//         </section>
+//       </main>
+//     );
+//   }
+// }
 
 // import React, { useEffect } from "react";
-// import { gsap } from "gsap";
-// import { Link } from "react-router-dom";
-// import MugBlack from "../img/store/pixelbakery-mug-black.webp";
-// import products from "../data/store-products";
+
 // import ProductCard from "../components/pg-store/store-product-card";
 // function Store() {
 //   useEffect(() => {
@@ -51,11 +257,11 @@ export default ProductFeed;
 //   }, []);
 
 //   return (
-//     <main>
-//       <section id='store' className='my-4 bg-blue-dark py-40'>
-//         <div className='mx-auto max-w-6xl'>
-//           <h1 className='text-pink mb-20 '>Company Shop</h1>
-//           <div className=' grid grid-cols-3 mt-8 gap-12'>
+//
+//
+//
+//
+//
 //             {products.map((product) => (
 //               // <ComponentName prop1={prop1Input} key={dataItem.identifier} />
 //               <ProductCard
