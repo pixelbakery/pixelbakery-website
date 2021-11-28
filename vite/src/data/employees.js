65fa880e158531b45bd3cc118e43a8b87066970d
Henry Sipp <hesipp@gmail.com>
2021-11-28 16:43:25 -0600

    ?

--- vite/src/data/employees.js
+++
@@ -1,9 +0,0 @@
-export default[
-    {
-        name: "Jordan Lambrecht",
-        title: "Director + Founder",
-        email: "jordan@pixelbakery.com",
-        img_headshot: "",
-        img_blunder: "",
-    }
-]
--- vite/src/lib/Commerce.jsx
+++
@@ -1,6 +0,0 @@
-import Commerce from "@chec/commerce.js";
-
-export const commerce = new Commerce(
-  "pk_36448d4fc74d71c64e4d8ef1e1f2947a35c7cc6cff143",
-  true
-);
