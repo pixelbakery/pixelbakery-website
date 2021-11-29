76e804090ac3be4dd46bc064f4e435c01e72c47f
Henry Sipp <hesipp@gmail.com>
2021-11-29 15:58:32 -0600

    Types

--- pages/about/[person].tsx
+++ pages/about/[person].tsx
@@ -1,9 +1,9 @@
-import type { NextPage, NextPageContext } from 'next'
+import type { GetStaticPaths, GetStaticProps, NextPage, NextPageContext } from 'next'
 import { useRouter } from 'next/router'
 import roster from '../../data/team'
 import TeamBio from '../../components/pg-about/about-bios'
 
-export async function getStaticPaths() {
+export let getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: roster.map((person) => ({
       params: {
@@ -14,7 +14,7 @@ export async function getStaticPaths() {
   }
 }
 
-export function getStaticProps(context: any) {
+export let getStaticProps: GetStaticProps = (context: any) => {
   const { person } = context.params
   return {
     props: {
@@ -26,11 +26,7 @@ export function getStaticProps(context: any) {
 let AboutPerson: NextPage = ({ data }) => {
   return (
     <div className='bg-egg p-8'>
-      {/* <h1>{data.name}</h1>
-      <h3>{data.bio}</h3> */}
-      <TeamBio {...data}
-            />         
-      {/* {JSON.stringify(data)} */}
+      <TeamBio {...data} />         
     </div>
   )
 }
