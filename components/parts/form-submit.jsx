f56fa83d9a646babe7338a4ebb045afc405ad754
Henry Sipp <hesipp@gmail.com>
2021-11-29 15:52:33 -0600

    Layout

--- components/parts/form-submit.jsx
+++ components/parts/form-submit.jsx
@@ -3,7 +3,7 @@ import { ChevronRightIcon } from '@heroicons/react/solid'
 
 export default function ButtonFilled({ text, link, color, chevronColor }) {
   return (
-    <input
+    <button
       type='submit'
       className={
         'flex font-semibold  lowercase py-4 rounded-md px-6 border-2 bg-transparent text-lg text-center border-' +
@@ -15,6 +15,6 @@ export default function ButtonFilled({ text, link, color, chevronColor }) {
       <i className='h-12 w-12 flex flex-col justify-center'>
         <ChevronRightIcon className={'text-opacity-50  text-' + chevronColor} />
       </i>
-    </input>
+    </button>
   )
 }
