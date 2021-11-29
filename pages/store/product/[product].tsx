e0317c7711210411e03bfb11cbfd420dcc5bfb31
Henry Sipp <hesipp@gmail.com>
2021-11-29 10:18:32 -0600

    Cart

--- pages/store/product/[product].tsx
+++ pages/store/product/[product].tsx
@@ -1,11 +1,11 @@
-import { NextPage } from 'next'
-import React, { useState } from 'react'
+import { GetServerSideProps, NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
 import Navigation from '../../../components/Navigation'
 import PageSection from '../../../components/PageSection'
 import commerce from '../../../lib/commerce'
 import Image from 'next/image'
 
-export async function getServerSideProps(context) {
+export let getServerSideProps: GetServerSideProps = async (context) => {
   const { product: permalink } = context.params
   const product = await commerce.products.retrieve(permalink, { type: 'permalink ' })
   return {
@@ -15,16 +15,26 @@ export async function getServerSideProps(context) {
   }
 }
 
-let StoreProduct: NextPage = ({ product }) => {
+let StoreProduct: NextPage = ({ product }: { product: any }) => {
   console.log({ product })
+  const [cart, setCart] = useState(null)
   const [price, setPrice] = useState(product.price.raw)
   const onPriceChange = (evt) => setPrice(evt.target.value)
+  const addToCart = async () => {
+    const result = await commerce.cart.add(product.id, 1)
+    console.log(result)
+    commerce.cart.retrieve().then(setCart)
+  }
+
+  useEffect(() => {
+    commerce.cart.retrieve().then(setCart)
+  }, [])
   return (
     <>
       {/* <Navigation /> */}
       <main id='' className='lander bg-white relative'>
-        {/* {JSON.stringify(product)} */}
         <PageSection className='inset-0'>
+          Cart items: {cart.total_items}
           <div className='grid grid-cols-1 items-center md:grid-cols-2 gap-16'>
             <div className='col-span-1 relative' style={{ minHeight: '50vh' }}>
               <Image
@@ -43,6 +53,7 @@ let StoreProduct: NextPage = ({ product }) => {
                 <input type='number' value={price} onChange={onPriceChange} />
               </div>
               <div dangerouslySetInnerHTML={{ __html: product.description }} />
+              <button onClick={addToCart}>Add to Cart</button>
             </div>
           </div>
         </PageSection>
