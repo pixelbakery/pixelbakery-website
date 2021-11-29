ebf72aefcef054dc59a18c02186c7dd4c233579e
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-29 13:37:13 -0600

    🚧 added prop to pass inner section max width

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -2,18 +2,20 @@ import cs from 'classnames'
 type Props = {
   className?: string
   innerClassName?: string
+  innerMaxWidth?: string
   id?: string
 }
 export default function PageSection({
   className,
   innerClassName,
+  innerMaxWidth,
   children,
   id,
   ...rest
 }: React.PropsWithChildren<Props>) {
   return (
     <section className={cs('py-8 lg:py-16', className)} id={id}>
-      <div className='max-w-screen-sm md:max-w-screen-md lg:max-w-6xl mx-auto'>
+      <div className={'mx-auto ' + innerMaxWidth}>
         <div className={cs('px-8 lg:px-12 py-8 ', innerClassName)}>{children}</div>
       </div>
     </section>
