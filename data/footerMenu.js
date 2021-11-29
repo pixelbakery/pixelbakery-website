2ec9b820eedb68b6a016b467a33fa47364c6110a
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-28 20:21:04 -0600

    🚢 continued migrating to nextjs

--- data/footerMenu.js
+++ data/footerMenu.js
@@ -1,28 +1,27 @@
-export default[
-    {
-        text: "Work",
-        url: "/work",
-    },
-    {
-        text: "About",
-        url: "/about",
-    },
-    {
-        text: "Services",
-        url: "/services",
-    },
-    {
-        text: "Education",
-        url: "/education",
-    },
-    {
-        text: "Contact Us",
-        url: "/contact",
-    },
-    {
-        text: "Mom's Recipes",
-        url: "/recipess",
-    },
-    
-
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    text: 'Work',
+    url: '/work',
+  },
+  {
+    text: 'About',
+    url: '/about',
+  },
+  {
+    text: 'Services',
+    url: '/services',
+  },
+  {
+    text: 'Education',
+    url: '/education',
+  },
+  {
+    text: 'Contact Us',
+    url: '/contact',
+  },
+  {
+    text: "Mom's Recipes",
+    url: '/recipes',
+  },
 ]
--- data/socialLinks.js
+++ data/socialLinks.js
@@ -1,52 +1,52 @@
 //TODO: Add actual URLS
-
+/* eslint-disable import/no-anonymous-default-export */
 export default [
-     {
-          name: 'instagram',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'youtube',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'discord',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'medium',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'twitter',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'tiktok',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'vimeo',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'twitch',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'minecraft',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'behance',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'facebook',
-          url: 'https://instagram.com/pixelbakery',
-     },
-     {
-          name: 'linkedin',
-          url: 'https://instagram.com/pixelbakery',
-     },
+  {
+    name: 'instagram',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'youtube',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'discord',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'medium',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'twitter',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'tiktok',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'vimeo',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'twitch',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'minecraft',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'behance',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'facebook',
+    url: 'https://instagram.com/pixelbakery',
+  },
+  {
+    name: 'linkedin',
+    url: 'https://instagram.com/pixelbakery',
+  },
 ]
