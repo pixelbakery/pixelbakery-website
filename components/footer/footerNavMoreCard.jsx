bc9e535f18f2396c9e52103cfc9156a1841e319b
Jordan Lambrecht <jordan@pixelbakery.co>
2021-11-29 21:55:24 -0600

    📱 mobile responsiveness

--- components/footer/footerNavMoreCard.jsx
+++ components/footer/footerNavMoreCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
-export default function FooterNavMoreCard({ navitem }){
-    return (
-        <li className="text-left" ><a className="text-cream text-xl" href={navitem.url}>{navitem.text}</a></li>
-    );
+export default function FooterNavMoreCard({ navitem }) {
+  return (
+    <li className='text-center md:text-left'>
+      <a className='text-cream text-xl' href={navitem.url}>
+        {navitem.text}
+      </a>
+    </li>
+  )
 }
