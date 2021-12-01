96f6ecc46ec6f808461057529044a277956ae907
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-01 16:01:00 -0600

    🧹 code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/typography.css'
 import '../styles/globals.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import type { AppProps } from 'next/app'
 
-
 const client = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <QueryClientProvider client={client}>
-  <Component {...pageProps} />
-  </QueryClientProvider>
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
