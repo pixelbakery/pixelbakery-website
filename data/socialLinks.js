2d2dbe945709192ba996771f81cec2ec0a86e601
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-01 16:00:28 -0600

    🦶 updated social links in footer

--- data/socialLinks.js
+++ data/socialLinks.js
@@ -4,49 +4,60 @@ export default [
   {
     name: 'instagram',
     url: 'https://instagram.com/pixelbakery',
+    icon: 'bi-instagram ',
   },
   {
     name: 'youtube',
-    url: 'https://instagram.com/pixelbakery',
+    url: 'https://www.youtube.com/c/pixelbakery',
+    icon: 'bi-youtube ',
   },
   {
     name: 'discord',
-    url: 'https://instagram.com/pixelbakery',
+    url: 'https://discord.gg/ZJfGg76BUB',
+    icon: 'bi-discord ',
   },
-  {
-    name: 'medium',
-    url: 'https://instagram.com/pixelbakery',
-  },
-  {
-    name: 'twitter',
-    url: 'https://instagram.com/pixelbakery',
-  },
-  {
-    name: 'tiktok',
-    url: 'https://instagram.com/pixelbakery',
-  },
+  // {
+  //   name: 'medium',
+  //   url: 'https://pixelbakery.medium.com',
+  //   icon: 'bi-medium ',
+  // },
+  // {
+  //   name: 'twitter',
+  //   url: 'https://twitter.com/PixelBakeryLNK/',
+  //   icon: 'bi-twitter ',
+  // },
+  // {
+  //   name: 'tiktok',
+  //   url: 'https://www.tiktok.com/@pixelbakery',
+  //   icon: 'bi-tiktok ',
+  // },
   {
     name: 'vimeo',
-    url: 'https://instagram.com/pixelbakery',
+    url: 'https://vimeo.com/pixelbakery',
+    icon: 'bi-vimeo ',
   },
   {
     name: 'twitch',
-    url: 'https://instagram.com/pixelbakery',
+    url: 'https://twitch.tv/pixelbakery',
+    icon: 'bi-twitch ',
   },
-  {
-    name: 'minecraft',
-    url: 'https://instagram.com/pixelbakery',
-  },
-  {
-    name: 'behance',
-    url: 'https://instagram.com/pixelbakery',
-  },
-  {
-    name: 'facebook',
-    url: 'https://instagram.com/pixelbakery',
-  },
+  // {
+  //   name: 'minecraft',
+  //   url: 'https://instagram.com/pixelbakery',
+  // },
+  // {
+  //   name: 'behance',
+  //   url: 'https://www.behance.net/pixelbakerylnk',
+  //   icon: 'bi-behance ',
+  // },
+  // {
+  //   name: 'facebook',
+  //   url: 'https://www.facebook.com/pixelbakerylnk/',
+  //   icon: 'bi-facebook ',
+  // },
   {
     name: 'linkedin',
-    url: 'https://instagram.com/pixelbakery',
+    url: 'https://www.linkedin.com/company/pixelbakery',
+    icon: 'bi-linkedin ',
   },
 ]
