7272e7164d4de29dc501cfc284fab9cd78bbe0c6
Henry Sipp <hesipp@gmail.com>
2021-11-30 22:20:20 -0600

    Stripe stuff

--- pages/store/checkout/index.tsx
+++ pages/store/checkout/index.tsx
@@ -13,6 +13,7 @@ import {
 import { loadStripe } from '@stripe/stripe-js'
 import { Formik } from 'formik'
 import BillingAddressForm from '../../../components/pg-store/BillingAddressForm'
+import { Stripe } from 'stripe'
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE)
 
@@ -26,32 +27,35 @@ let Checkout: NextPage = () => {
     setFurthest(Math.max(furthestStep, step))
   }, [step])
 
-  const [clientSecret, setClientSecret] = useState('')
   const [billingSameAsShipping, setBillingSameAsShipping] = useState(false)
 
   const [shippingAddress, setShippingAddress] = useState({})
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch('/api/create-payment-intent', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
-    })
-      .then(async (res) => {
-        const a = await res.json()
-        console.log('asdf', a)
-        return a
-      })
-      .then((data) => setClientSecret(data.clientSecret))
-  }, [])
-
-  if (!clientSecret) {
-    return null
-  }
+  const stripe = useStripe()
+  const elements = useElements()
 
   return (
     <Formik
-      onSubmit={(values) => {
+      onSubmit={async (values) => {
+        if (!stripe || !elements) {
+          return
+        }
+        const { error } = await stripe.confirmPayment({
+          elements,
+          confirmParams: {
+            // Make sure to change this to your payment completion page
+            return_url: 'http://localhost:3000',
+          },
+        })
+
+        // const paymentMethodResponse = await stripe.createPaymentMethod({ type: 'card', card });
+
+        // if (paymentMethodResponse.error) {
+        //   // There was some issue with the information that the customer entered into the payment details form.
+        //   alert(paymentMethodResponse.error.message)
+        //   return
+        // }
+
+        const valuesToSubmit = {}
         console.log(values)
       }}
       initialValues={{
@@ -72,76 +76,98 @@ let Checkout: NextPage = () => {
       }}
     >
       {({ values, handleChange, handleSubmit }) => (
-        <Elements
-          options={{
-            clientSecret,
-          }}
-          stripe={stripePromise}
-        >
-          <main className='min-lander p-4'>
-            <div className='max-w-screen-lg flex flex-row items-start gap-12'>
-              <div className='flex-1'>
-                <div className='p-4 border-b'>
-                  <h3>Shipping</h3>
-                  <ShippingAddressForm />
-                </div>
-                <div className='p-4 border-b'>
-                  <h3>Billing</h3>
+        <main className='min-lander p-4'>
+          <div className='max-w-screen-lg flex flex-row items-start gap-12'>
+            <div className='flex-1'>
+              <div className='p-4 border-b'>
+                <h3>Shipping</h3>
+                <ShippingAddressForm />
+              </div>
+              <div className='p-4 border-b'>
+                <h3>Billing</h3>
+                <div>
                   <div>
-                    <div>
-                      <label>Same as shipping</label>
-                      <input
-                        type='checkbox'
-                        value={billingSameAsShipping}
-                        onChange={() => {
-                          setBillingSameAsShipping(!billingSameAsShipping)
-                        }}
-                      />
-                    </div>
-                    {billingSameAsShipping ? null : <BillingAddressForm />}
+                    <label>Same as shipping</label>
+                    <input
+                      type='checkbox'
+                      value={billingSameAsShipping}
+                      onChange={() => {
+                        setBillingSameAsShipping(!billingSameAsShipping)
+                      }}
+                    />
+                  </div>
+                  {billingSameAsShipping ? null : <BillingAddressForm />}
 
-                    <select className='block'>
-                      {token?.shipping_methods?.map((opt) => (
-                        <option value={opt.id} key={opt.id}>
-                          {opt.description} {opt.price.formatted_with_symbol}
-                        </option>
-                      ))}
-                    </select>
-                    {/* <ShippingOptions /> */}
-                    <button onClick={() => setStep(3)}>Next</button>
-                  </div>
-                </div>
-                <div className='p-4'>
-                  <h3>Payment</h3>
-                  <div>
-                    {/*
-                     */}
-                    <PaymentElement />
-                  </div>
+                  <select className='block'>
+                    {token?.shipping_methods?.map((opt) => (
+                      <option value={opt.id} key={opt.id}>
+                        {opt.description} {opt.price.formatted_with_symbol}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
-
-              <div className='sticky flex-1 top-12 p-4 bg-blue-light'>
-                <p>Cart info here</p>
-                {cart?.line_items.map((item) => (
-                  <div key={item.id} className='flex flex-row items-center'>
-                    <p>
-                      {item.name} x{item.quantity}
-                    </p>
-                    <div className='flex-1'></div>
-                    <p>{item?.line_total?.formatted_with_symbol}</p>
-                  </div>
-                ))}
-                <h3>{cart?.subtotal.formatted_with_symbol}</h3>
+              <div className='p-4'>
+                <h3>Payment</h3>
+                <div>
+                  {/*
+                   */}
+                  <PaymentElement />
+                </div>
               </div>
             </div>
-            <button type='submit' onClick={handleSubmit}>
-              Submit
-            </button>
-          </main>
-        </Elements>
+
+            <div className='sticky flex-1 top-12 p-4 bg-blue-light'>
+              <p>Cart info here</p>
+              {cart?.line_items.map((item) => (
+                <div key={item.id} className='flex flex-row items-center'>
+                  <p>
+                    {item.name} x{item.quantity}
+                  </p>
+                  <div className='flex-1'></div>
+                  <p>{item?.line_total?.formatted_with_symbol}</p>
+                </div>
+              ))}
+              <h3>{cart?.subtotal.formatted_with_symbol}</h3>
+            </div>
+          </div>
+          <button type='submit' onClick={handleSubmit}>
+            Submit
+          </button>
+        </main>
       )}
     </Formik>
   )
 }
-export default Checkout
+export default function CheckoutPage() {
+  const [clientSecret, setClientSecret] = useState('')
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch('/api/create-payment-intent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
+    })
+      .then(async (res) => {
+        const a = await res.json()
+        console.log('asdf', a)
+        return a
+      })
+      .then((data) => setClientSecret(data.clientSecret))
+  }, [])
+
+  if (!clientSecret) {
+    return null
+  }
+  return (
+    <Elements
+      options={{
+        clientSecret,
+      }}
+      stripe={stripePromise}
+    >
+      <Checkout />
+    </Elements>
+  )
+}
