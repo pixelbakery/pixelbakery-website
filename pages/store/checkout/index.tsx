85d0342ba9c62c818508b6ca591a772d5fb632fe
Henry Sipp <hesipp@gmail.com>
2021-11-30 21:38:14 -0600

    Forms

--- pages/store/checkout/index.tsx
+++ pages/store/checkout/index.tsx
@@ -11,6 +11,8 @@ import {
   CardElement,
 } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+import { Formik } from 'formik'
+import BillingAddressForm from '../../../components/pg-store/BillingAddressForm'
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE)
 
@@ -48,71 +50,98 @@ let Checkout: NextPage = () => {
   }
 
   return (
-    <Elements
-      options={{
-        clientSecret,
+    <Formik
+      onSubmit={(values) => {
+        console.log(values)
       }}
-      stripe={stripePromise}
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        shipping: {
+          address: '',
+          city: '',
+          state: '',
+          postalCode: '',
+        },
+        billing: {
+          address: '',
+          city: '',
+          state: '',
+          postalCode: '',
+        },
+      }}
     >
-      <main className='min-lander p-4'>
-        <div className='max-w-screen-lg flex flex-row items-start gap-12'>
-          <div className='flex-1'>
-            <div className='p-4 border-b'>
-              <h3>Shipping</h3>
-              <ShippingAddressForm onSubmit={() => setStep(2)} />
-            </div>
-            <div className='p-4 border-b'>
-              <h3>Billing</h3>
-              <div>
-                <div>
-                  <label>Same as shipping</label>
-                  <input
-                    type='checkbox'
-                    value={billingSameAsShipping}
-                    onChange={() => {
-                      setBillingSameAsShipping(!billingSameAsShipping)
-                    }}
-                  />
+      {({ values, handleChange, handleSubmit }) => (
+        <Elements
+          options={{
+            clientSecret,
+          }}
+          stripe={stripePromise}
+        >
+          <main className='min-lander p-4'>
+            <div className='max-w-screen-lg flex flex-row items-start gap-12'>
+              <div className='flex-1'>
+                <div className='p-4 border-b'>
+                  <h3>Shipping</h3>
+                  <ShippingAddressForm />
                 </div>
-                {billingSameAsShipping ? null : <ShippingAddressForm onSubmit={() => setStep(2)} />}
+                <div className='p-4 border-b'>
+                  <h3>Billing</h3>
+                  <div>
+                    <div>
+                      <label>Same as shipping</label>
+                      <input
+                        type='checkbox'
+                        value={billingSameAsShipping}
+                        onChange={() => {
+                          setBillingSameAsShipping(!billingSameAsShipping)
+                        }}
+                      />
+                    </div>
+                    {billingSameAsShipping ? null : <BillingAddressForm />}
 
-                <select className='block'>
-                  {token?.shipping_methods?.map((opt) => (
-                    <option value={opt.id} key={opt.id}>
-                      {opt.description} {opt.price.formatted_with_symbol}
-                    </option>
-                  ))}
-                </select>
-                {/* <ShippingOptions /> */}
-                <button onClick={() => setStep(3)}>Next</button>
+                    <select className='block'>
+                      {token?.shipping_methods?.map((opt) => (
+                        <option value={opt.id} key={opt.id}>
+                          {opt.description} {opt.price.formatted_with_symbol}
+                        </option>
+                      ))}
+                    </select>
+                    {/* <ShippingOptions /> */}
+                    <button onClick={() => setStep(3)}>Next</button>
+                  </div>
+                </div>
+                <div className='p-4'>
+                  <h3>Payment</h3>
+                  <div>
+                    {/*
+                     */}
+                    <PaymentElement />
+                  </div>
+                </div>
+              </div>
+
+              <div className='sticky flex-1 top-12 p-4 bg-blue-light'>
+                <p>Cart info here</p>
+                {cart?.line_items.map((item) => (
+                  <div key={item.id} className='flex flex-row items-center'>
+                    <p>
+                      {item.name} x{item.quantity}
+                    </p>
+                    <div className='flex-1'></div>
+                    <p>{item?.line_total?.formatted_with_symbol}</p>
+                  </div>
+                ))}
+                <h3>{cart?.subtotal.formatted_with_symbol}</h3>
               </div>
             </div>
-            <div className='p-4'>
-              <h3>Payment</h3>
-              <div>
-                {/*
-                 */}
-                <PaymentElement />
-              </div>
-            </div>
-          </div>
-
-          <div className='sticky flex-1 top-12 p-4 bg-blue-light'>
-            <p>Cart info here</p>
-            {cart?.line_items.map((item) => (
-              <div key={item.id} className='flex flex-row items-center'>
-                <p>
-                  {item.name} x{item.quantity}
-                </p>
-                <div className='flex-1'></div>
-                <p>{item?.line_total?.formatted_with_symbol}</p>
-              </div>
-            ))}
-            <h3>{cart?.subtotal.formatted_with_symbol}</h3>
-          </div>
-        </div>
-      </main>
-    </Elements>
+            <button type='submit' onClick={handleSubmit}>
+              Submit
+            </button>
+          </main>
+        </Elements>
+      )}
+    </Formik>
   )
 }
 export default Checkout
