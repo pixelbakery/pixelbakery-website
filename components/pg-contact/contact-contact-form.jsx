4b9bb1a9dd44ef0fbf3805d73aa2cdeab5e4451f
Henry Sipp <hesipp@gmail.com>
2021-12-01 23:08:32 -0600

    Why broken

--- components/pg-contact/contact-contact-form.jsx
+++ components/pg-contact/contact-contact-form.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
-function ContactForm(){
-     return(
-          <section className="my-4 bg-pink-light mx-auto py-6">
-               <div className="max-w-6xl">
-                    <h2 className="text-pink">HMU.</h2>
-                    Contact Form
-               </div>
-          </section>
-     )
+function ContactForm() {
+  return (
+    <section className='my-4 bg-pink-light mx-auto py-6'>
+      <div className='max-w-6xl'>
+        <h2 className='text-pink'>HMU.</h2>
+        Contact Form
+      </div>
+    </section>
+  )
 }
 export default ContactForm
