1674593738c618ecb82f0c99f2cbd8ee88f8d642
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-01 20:12:06 -0600

    im scared and fucked everything up. resetting

--- components/pg-store/BillingAddressForm.tsx
+++ components/pg-store/BillingAddressForm.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Formik, useFormik, useFormikContext } from 'formik'
 import commerce from '../../lib/commerce'
 
-export default function BillingAddressForm({ onSubmit }: { onSubmit: any }) {
+export default function BillingAddressForm({ onSubmit }) {
   const [states, setStates] = useState({})
   useEffect(() => {
     commerce.services.localeListSubdivisions('US').then((res) => setStates(res.subdivisions))
