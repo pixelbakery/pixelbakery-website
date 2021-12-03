9b8362dc1ede6dd4d8c506676d79080a7d129d9a
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-02 19:11:56 -0600

    redirect all trafic to store page for now

--- data/footerMenuMore.js
+++ data/footerMenuMore.js
@@ -3,34 +3,31 @@
 export default [
   {
     text: 'Privacy Policy',
-    url: '/privacy-policy',
-  },
-  {
-    text: 'Careers',
-    url: '/careers',
-  },
-  {
-    text: 'Bakery Login',
-    url: '/login',
-  },
-  {
-    text: 'Press Kit',
-    url: '/press-kit',
-  },
-  {
-    text: 'Cookbook',
-    url: '/cookbook',
-  },
-  {
-    text: 'Motion Mixer',
-    url: '/motion-mixer',
-  },
-  {
-    text: 'Company Store',
     url: '/store',
   },
   {
+    text: 'Careers',
+    url: '/store',
+  },
+  {
+    text: 'Bakery Login',
+    url: '/store',
+  },
+  {
+    text: 'Press Kit',
+    url: '/store',
+  },
+  {
+    text: 'Cookbook',
+    url: '/store',
+  },
+  {
+    text: 'Motion Mixer',
+    url: '/store',
+  },
+
+  {
     text: 'Pay-What-Ya-Want Web Hosting',
-    url: '/web-hosting',
+    url: '/store',
   },
 ]
