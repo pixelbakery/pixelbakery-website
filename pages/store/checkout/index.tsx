2a80d07e77eff37f2ce4f22ccef842f31b2d6715
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-04 13:06:46 -0600

    CHANGED: checkout forms will no longer display if cart is empty

--- pages/store/checkout/index.tsx
+++ pages/store/checkout/index.tsx
@@ -232,118 +232,130 @@ let Checkout: NextPage = () => {
             <meta name='twitter:card' content='summary_large_image'></meta>
             <meta name='twitter:image:alt' content='Pixel Bakery Design Studio'></meta>
           </Head>
-          <section className='mx-auto max-w-6xl px-6 md:px-12'>
-            <LoadingModal visible={loading} />
-            <header className='mb-6 pb-8 mt-12'>
-              <div id='breadcrumbs' className='w-full text-blue  text-sm '>
-                <Link href='/store' passHref>
-                  <a className='underline py-2'>store</a>
-                </Link>
-                <span className='mx-3 py-2'>/</span>
-                <Link href='/store/cart' passHref>
-                  <a className='underline py-2'>cart</a>
-                </Link>
-                <span className='mx-3 py-3'>/</span>
-                <span className='py-3'>payment & shipping</span>
-              </div>
-              <h1 className='text-peach text-6xl lg:text-8xl pt-4'> Payment & Shipping</h1>
-            </header>
-            <div className='w-full grid grid-cols-5 items-start gap-12 '>
-              <div className='col-span-5 lg:col-span-3'>
-                {/* 
+          {cart?.total_items > 0 ? (
+            <section className='mx-auto max-w-6xl px-6 md:px-12'>
+              <LoadingModal visible={loading} />
+              <header className='mb-6 pb-8 mt-12'>
+                <div id='breadcrumbs' className='w-full text-blue  text-sm '>
+                  <Link href='/store' passHref>
+                    <a className='underline py-2'>store</a>
+                  </Link>
+                  <span className='mx-3 py-2'>/</span>
+                  <Link href='/store/cart' passHref>
+                    <a className='underline py-2'>cart</a>
+                  </Link>
+                  <span className='mx-3 py-3'>/</span>
+                  <span className='py-3'>payment & shipping</span>
+                </div>
+                <h1 className='text-peach text-6xl lg:text-8xl pt-4'> Payment & Shipping</h1>
+              </header>
+              <div className='w-full grid grid-cols-5 items-start gap-12 '>
+                <div className='col-span-5 lg:col-span-3'>
+                  {/* 
 
                   Shipping info
 
                  */}
-                <div className='px-4 pb-12 border-b-4 border-blue-dark '>
-                  <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Shipping</h2>
-                  <ShippingAddressForm />
-                </div>
-
-                {/* 
+                  <div className='px-4 pb-12 border-b-4 border-blue-dark '>
+                    <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Shipping</h2>
+                    <ShippingAddressForm />
+                  </div>
+                  {/* 
 
                   Billing info
 
                 */}
-                <div className='py-12 border-b-4 border-blue-dark'>
-                  <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Billing</h2>
-                  <div>
-                    <div className='my-4'>
-                      <input
-                        type='checkbox'
-                        className=' mr-3 border-blue text-blue p-2 cursor-pointer rounded-sm'
-                        checked={billingSameAsShipping}
-                        onChange={() => {
-                          setBillingSameAsShipping(!billingSameAsShipping)
-                        }}
-                      />
-                      <label className='text-wine align-middle'>Same as shipping</label>
-                    </div>
-                    <div className='my-4'>
-                      {billingSameAsShipping ? null : <BillingAddressForm />}
+                  <div className='py-12 border-b-4 border-blue-dark'>
+                    <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Billing</h2>
+                    <div>
+                      <div className='my-4'>
+                        <input
+                          type='checkbox'
+                          className=' mr-3 border-blue text-blue p-2 cursor-pointer rounded-sm'
+                          checked={billingSameAsShipping}
+                          onChange={() => {
+                            setBillingSameAsShipping(!billingSameAsShipping)
+                          }}
+                        />
+                        <label className='text-wine align-middle'>Same as shipping</label>
+                      </div>
+                      <div className='my-4'>
+                        {billingSameAsShipping ? null : <BillingAddressForm />}
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                {/* 
+                  {/* 
 
                   Shipping method
 
                  */}
-                <div className='py-12 border-b-4 border-blue-dark'>
-                  <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Shipping Options</h2>
-                  <select
-                    className='block form-border-b'
-                    value={shippingMethod}
-                    onChange={onShippingChange}
-                  >
-                    <option value={''}>select</option>
-                    {token?.shipping_methods?.map((opt) => (
-                      <option value={opt.id} key={opt.id}>
-                        {opt.description} {opt.price.formatted_with_symbol}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-
-                {/* 
+                  <div className='py-12 border-b-4 border-blue-dark'>
+                    <h2 className='mt-0 pt-0 text-2xl text-blue-dark'>Shipping Options</h2>
+                    <select
+                      className='block form-border-b'
+                      value={shippingMethod}
+                      onChange={onShippingChange}
+                    >
+                      <option value={''}>select</option>
+                      {token?.shipping_methods?.map((opt) => (
+                        <option value={opt.id} key={opt.id}>
+                          {opt.description} {opt.price.formatted_with_symbol}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  {/* 
 
                   Stripe
 
                  */}
-
-                {/* <h1>
+                  {/* <h1>
                   asdcvbnm {live?.pay_what_you_want.customer_set_price?.raw} {cartMin}
                 </h1> */}
-                <div className='py-4-12'>
-                  <h2 className='text-2xl text-blue-dark'>payment</h2>
-                  <div className='py-8 px-4 my-6 border border-blue rounded-md'>
-                    <CardElement className='text-2xl h-full' />
+
+                  <h1>{token?.shipping} </h1>
+                  <div className='py-4-12'>
+                    <h2 className='text-2xl text-blue-dark'>payment</h2>
+                    <div className='py-8 px-4 my-6 border border-blue rounded-md'>
+                      <CardElement className='text-2xl h-full' />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              {/* 
+                {/* 
                 Cart Details 
               */}
-              <CartDetails
-                cartMin={cartMin}
-                cartMax={maxCost}
-                cost={cost}
-                onCostChange={onCostChange}
-              />
-            </div>
-            {/* End Cart Details */}
+                <CartDetails
+                  cartMin={cartMin}
+                  cartMax={maxCost}
+                  cost={cost}
+                  onCostChange={onCostChange}
+                />
+              </div>
+              {/* End Cart Details */}
 
-            <button
-              type='submit'
-              onClick={() => handleSubmit()}
-              className='block text-right bg-peach py-4 px-12 my-8 text-pink-light rounded-lg text-2xl transform transition-all duration-600 ease-in-out scale-100 opacity-100
+              <button
+                type='submit'
+                onClick={() => handleSubmit()}
+                className='block text-right bg-peach py-4 px-12 my-8 text-pink-light rounded-lg text-2xl transform transition-all duration-600 ease-in-out scale-100 opacity-100
   hover:opacity-90 hover:scale-99 active:scale-97'
-            >
-              submit
-            </button>
-          </section>
+              >
+                submit
+              </button>
+            </section>
+          ) : (
+            <section className='bg-white h-96 flex flex-col justify-center'>
+              <div className='max-w-6xl mx-auto'>
+                <h3 className='text-wine text-xl italic font-semibold text-opacity-40'>
+                  well, that&apos;s a sad looking cart.{' '}
+                  <Link href='/store' passHref>
+                    <a className='underline text-blue'>make it less sad</a>
+                  </Link>
+                  .
+                </h3>
+              </div>
+            </section>
+          )}
           <Maintenance />
         </main>
       )}
