ecf26944aa9e613f99f5a9596a8062e6436d9c3b
Henry Sipp <hesipp@gmail.com>
2021-12-04 13:47:06 -0600

    Refactor state

--- components/pg-store/CartDetails.tsx
+++ components/pg-store/CartDetails.tsx
@@ -3,6 +3,8 @@ import { Live } from '@chec/commerce.js/types/live'
 import Link from 'next/link'
 import { useEffect } from 'react'
 import { useCheckoutState } from '../../hooks/useCheckoutState'
+import useShippingSummary from '../../hooks/useShippingSummary'
+import useTotals from '../../hooks/useTotals'
 
 type Props = {
   cost: number
@@ -10,26 +12,28 @@ type Props = {
   cart?: Cart
   [key: string]: any
 }
-export default function CartDetails({ cartMin, cartMax, cost, onCostChange }: Props) {
-  const { cart, live, checkPWYW } = useCheckoutState()
+export default function CartDetails({ pwywMin, pwywMax, pwyw, onPwywChange }: Props) {
+  const { cart, live } = useCheckoutState()
+  const { subtotal, total, totalWithTax, totalDue, tax } = useTotals()
 
-  // const taxRate = live?.tax.breakdown
-  //   .reduce((prev, next) => {
-  //     return prev + (next?.rate ?? 0)
-  //   }, 0)
-  //   .toFixed(2)
+  const customer_set_price = live?.pay_what_you_want?.customer_set_price?.raw
 
-  const val = live?.pay_what_you_want?.customer_set_price?.raw
+  const shipping = useShippingSummary()
 
-  const pwyw = val && val > 0 ? val : undefined
-  const price = pwyw ?? live?.subtotal.raw
+  let actualTotal = totalDue.raw + tax?.amount?.raw
 
+  console.log({ subtotal, total, totalWithTax, totalDue, tax })
+
+  // if (customer_set_price && customer_set_price > 0) {
+  //   actualTotal = customer_set_price + tax.amount.raw
+  // }
+
+  // const val = live?.pay_what_you_want?.customer_set_price?.raw
+  // const pwyw = val && val > 0 ? val : undefined
+  // const price = pwyw ?? live?.subtotal.raw
   // console.log(live, live?.subtotal?.raw, val, pwyw, price)
-  // const tax = price * taxRate
-  const tax = live?.tax.amount.formatted_with_symbol
+  // const shipping = live?.shipping?.price?.raw ?? 0
 
-  const shipping = live?.shipping?.price?.raw ?? 0
-  const total = price // shipping + (price || 0) // + tax
   return (
     <div className='col-span-5 lg:col-span-2 lg:sticky top-12 px-4 md:px-8 py-8 bg-blue-light order-first lg:order-last'>
       <p className='text-blue-dark text-2xl font-bold mb-12 border-b-4 border-blue-dark pb-2 px-2 '>
@@ -67,10 +71,10 @@ export default function CartDetails({ cartMin, cartMax, cost, onCostChange }: Pr
       <div className='my-4 flex flex-col md:flex-row gap-6 items-center mt-8'>
         <input
           type='range'
-          min={live?.pay_what_you_want.minimum?.raw}
-          max={cartMax}
-          value={Number(cost ?? 0).toFixed(2)}
-          onChange={onCostChange}
+          min={pwywMin}
+          max={pwywMax}
+          value={Number(pwyw ?? 0).toFixed(2)}
+          onChange={onPwywChange}
           className='bg-blue slider w-full md:w-auto md:flex-grow'
         />
         <span>
@@ -79,24 +83,19 @@ export default function CartDetails({ cartMin, cartMax, cost, onCostChange }: Pr
           <input
             className='rounded-md border-blue bg-transparent font-medium font-wine w-24 text-left pl-2 hover:opacity-60 focus:ring-blue-dark focus:ring-1'
             type='number'
-            min={cartMin}
-            value={Number(cost ?? 0).toFixed(2)}
-            onChange={onCostChange}
+            min={pwywMin}
+            value={Number(pwyw ?? 0).toFixed(2)}
+            onChange={onPwywChange}
           />
         </span>
       </div>
-      <p className='text-wine text-right'>shipping {live?.shipping?.price.formatted_with_symbol}</p>
-      <p className='text-wine text-right italic py-2'>
-        tax {tax}
-        {/* <br />
-        {JSON.stringify(live?.tax)} */}
-      </p>
+      <p className='text-wine text-right'>shipping {shipping?.price.formatted_with_symbol}</p>
+      <p className='text-wine text-right italic py-2'>tax {tax.amount.formatted_with_symbol}</p>
       <p className='text-wine text-right italic py-2 text-opacity-60'>
         taxes will be calculated after you enter a billing address
       </p>
       <h3 className='mt-8 text-right font-semibold text-2xl text-wine'>
-        {(total ?? 0).toFixed(2)}
-        {/* {live?.pay_what_you_want?.customer_set_price?.formatted_with_symbol} */}
+        ${actualTotal.toFixed(2)}
       </h3>
     </div>
   )
