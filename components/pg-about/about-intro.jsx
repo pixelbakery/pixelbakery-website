11c243631a6b12e55b374c9fccec8d898eaa1053
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-04 23:28:45 -0600

    about intro page created

---
+++ components/pg-about/about-intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <section className='my-4' id='team'>
+      <div className='mx-auto grid gap-4 grid-cols-12'>
+        <div className='bg-blue aspect-w-3 aspect-h-4 '>
+          <div className='px-12 w-full  flex flex-col justify-center'>
+            <h2 className='col-span-3 text-blue-dark text-left mx-auto'>
+              who
+              <br className='hidden sm:block' /> we
+              <br className='hidden sm:block' /> are
+            </h2>
+          </div>
+        </div>
+        <div className=' col-span9'>
+          <h3 className='text-blue bg-yellow text-6xl max-w-3xl'>
+            we&apos;re a ragtag team of scrappy designers and producers deadset on these three
+            things
+          </h3>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Intro
