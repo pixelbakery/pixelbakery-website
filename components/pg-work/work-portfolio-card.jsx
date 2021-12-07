24e8a9406d3faa660af087b1aff83aca9ab605e7
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-07 10:53:27 -0600

    expanded portfolio section on work page

--- components/pg-work/work-portfolio-card.jsx
+++ components/pg-work/work-portfolio-card.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
-export default function ServiceCard({ project }) {
+import Link from 'next/link'
+import Pill from '../../components/parts/pill'
+import Scrim from '../../components/parts/scrim'
+
+export default function ServiceCard(props) {
   return (
-    <article
-      className={
-        ' portfolio-project aspect-w-3 xl:aspect-w-4 aspect-h-2 xl:aspect-h-3  w-full hover:opacity-90 hover:scale-95 ' +
-        project.isotopeTag
-      }
-      data-category={project.isotopeTag}
-    >
-      <div className='absolute w-full h-full'>
+    <Link href={props.url} passHref>
+      <article className='bg-blue aspect-h-9 aspect-w-16 cursor-pointer'>
+        {/* <Image src={previewImg} layout='fill' alt={previewImgAlt} /> */}
         <Image
-          src={project.previewImg}
-          className='w-full h-full object-center object-cover'
-          alt={project.previewImgAlt}
+          alt={props.previewImgAlt}
+          src={props.previewImg}
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+          className=''
         />
-      </div>
-      <div className='absolute w-full h-full p-2'>
-        <div className='px-12 py-4 bg-cream inline-block'>
-          <h3 className=' leading-none my-0 -pb-2  text-wine '>{project.client}</h3>
-          <h2 className='text-wine leading-none text-3xl py-0 my-0'>{project.name}</h2>
+        <Scrim />
+        <div className=' z-20 h-100 p-6 flex flex-col justify-between'>
+          <div className='-py-3 flex flex-wrap flex-row  gap-4'>
+            {props.tags.map((tag) => (
+              <Pill text={tag} bgColor={'peach'} textColor={'cream'} size='sm' key={tag} />
+            ))}
+          </div>
+          <div className=''>
+            <div className='text-sm text-white'>{props.client}</div>
+            <h3 className='text-2xl text-white'>{props.name}</h3>
+          </div>
         </div>
-      </div>
-      <a href='' className='absolute h-full w-full z-10 opacity-0 top-0 left-0 cursor-pointer'></a>
-    </article>
+      </article>
+    </Link>
   )
 }
