c83293f1f1170a23af05a7eef34d067482a6b254
Jordan Lambrecht <jordan@pixelbakery.co>
2021-12-07 10:52:41 -0600

    added scrim component

---
+++ components/parts/scrim.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import next from 'next'
+
+function ComponentName() {
+  return (
+    <div className='absolute w-100 h-100 bg-gradient-to-t  from-wine via-transparent opacity-25'></div>
+  )
+}
+
+export default ComponentName
