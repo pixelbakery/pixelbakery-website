e8add14c5eeba0bafbc372168ee8645c29adf9f0
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-10 14:22:01 -0600

    fixed mobile responsiveness issues

--- components/pg-education/edu-section-fromScratch.jsx
+++ components/pg-education/edu-section-fromScratch.jsx
@@ -7,7 +7,7 @@ import Section__Header from '../../components/pg-education/edu-section-header'
 function EducationL() {
   return (
     <section className='bg-white pt-16 pb-24  my-4 ' id='recentlyAdded'>
-      <div className='px-2 max-w-md lg:max-w-7xl  mx-auto px-4'>
+      <div className=' max-w-md lg:max-w-7xl  mx-auto px-4'>
         <h2 className='text-blue mb-16'>from scratch</h2>
         <p className='text-wine-200 font-semibold text-xl max-w-sm'>
           Proactively repurpose premier leadership via sticky architectures. Uniquely predominate
