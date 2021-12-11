d39fb35b815ef03a9da35c84d00a8cdebff40086
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-11 00:31:43 -0600

    debug

--- pages/post.jsx
+++ pages/post.jsx
@@ -6,7 +6,7 @@ import Pill from '../components/parts/pill'
 export default function Recipe() {
   return (
     <main className=' mt-44'>
-      {/* <article className='' id='blog-body'>
+      <article className='' id='blog-body'>
         <section className='md:max-w-3xl mx-auto '>
           <div className='' id='blog-body-intro'>
             <div className='mb-6'>
@@ -170,7 +170,7 @@ export default function Recipe() {
             </Link>
           </div>
         </div>
-      </section> */}
+      </section>
     </main>
   )
 }
