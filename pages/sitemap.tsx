9d3725c0508f00d27c53d255e5ff70ed9dfb53ac
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-11 16:07:02 -0600

    🧼  code cleanup

--- pages/sitemap.tsx
+++ pages/sitemap.tsx
@@ -10,21 +10,17 @@ function SiteMap() {
           <h1 className='text-6xl text-wine-200'>Quick Links</h1>
           <ul className='text-wine text-lg mt-8 py-3 pr-2 list-disc list-inside'>
             <li>
-              <Link
-                href='https://github.com/jordanlambrecht/pb-oct-2022'
-                className='underline'
-                target='_blank'
-              >
-                GitHub
+              <Link href='https://github.com/jordanlambrecht/pb-oct-2022' passHref>
+                <a className='underline' target='_blank'>
+                  GitHub
+                </a>
               </Link>
             </li>
             <li>
-              <Link
-                href='https://pixelbakery.monday.com/boards/911081678/'
-                className='underline'
-                target='_blank'
-              >
-                Monday Board
+              <Link href='https://pixelbakery.monday.com/boards/911081678/' passHref>
+                <a className='underline' target='_blank'>
+                  Monday Board
+                </a>
               </Link>
             </li>
           </ul>
