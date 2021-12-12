02dbbac32b6c02ea2e0a71bde6b4f292ec888458
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-11 20:42:42 -0600

    debug

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,7 +3,7 @@ import next from 'next'
 
 import About_Intro from '../../components/About/About_Intro'
 import About_Team from '../../components/About/About_Team'
-import About_Values from '../../components/About/About_Valuess'
+import About_Values from '../../components/About/About_Values'
 import About_StudioViews from '../../components/About/About_StudioViews'
 
 import About_Twitch from '../../components/About/About_Twitch'
