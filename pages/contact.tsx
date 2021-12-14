efe95b3abbc9f7ecef899e0e116b9f58ad702186
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-14 12:02:58 -0600

    🧼 code cleanup and file restructuring

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import Onboarding from '../components/Contact/Contact_Clients'
-import ContactForm from '../components/Contact/Contact_ContactForm'
-import Jobs from '../components/Contact/Contact_Jobs'
-import Hang from '../components/Contact/Contact_HangOut'
+import Contact_Clients from '../components/Contact/Contact_Clients'
+import Contact_ContactForm from '../components/Contact/Contact_ContactForm'
+import Contact_Jobs from '../components/Contact/Contact_Jobs'
+import Contact_HangOut from '../components/Contact/Contact_HangOut'
 
 function Contact() {
-  return <main className='' id='contact-page'></main>
+  return (
+    <main className='' id='contact-page'>
+      <Contact_Clients />
+      <Contact_ContactForm />
+      <Contact_Jobs />
+      <Contact_HangOut />
+    </main>
+  )
 }
 
 export default Contact
