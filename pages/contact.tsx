38ea141c892ba0d5377d8d45ccdfeeff0840fffc
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-14 12:39:39 -0600

    fixed bug with new social media icon components

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Contact_Clients from '../components/Contact/Contact_Clients'
 import Contact_ContactForm from '../components/Contact/Contact_ContactForm'
 import Contact_Jobs from '../components/Contact/Contact_Jobs'
-import Contact_HangOut from '../components/Contact/Contact_HangOut'
+// import Contact_HangOut from '../components/Contact/Contact_HangOut'
 
 function Contact() {
   return (
@@ -10,7 +10,7 @@ function Contact() {
       <Contact_Clients />
       <Contact_ContactForm />
       <Contact_Jobs />
-      <Contact_HangOut />
+      {/* <Contact_HangOut /> */}
     </main>
   )
 }
