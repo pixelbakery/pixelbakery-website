7f0878af64df00d586596e37ef2b996613593f29
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-18 14:41:00 -0600

    Disabling pages until I can read about dynamic content more

--- pages/education/index.tsx
+++ pages/education/index.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 //sections
 import Education_Header from '../../components/Education/Education_Header'
-import Education_Recent from '../../components/Education/Education_Recent'
-import Education_MadeToOrder from '../../components/Education/Education_MadeToOrder'
-import Education_FromScratch from '../../components/Education/Education_FromScratch'
-import Education_ProjectFiles from '../../components/Education/Education_ProjectFiles'
-import Education_TutorialRequests from '../../components/Education/Education_TutorialRequests'
+// import Education_Recent from '../../components/Education/Education_Recent'
+// import Education_MadeToOrder from '../../components/Education/Education_MadeToOrder'
+// import Education_FromScratch from '../../components/Education/Education_FromScratch'
+// import Education_ProjectFiles from '../../components/Education/Education_ProjectFiles'
+// import Education_TutorialRequests from '../../components/Education/Education_TutorialRequests'
 
 function EducationPage() {
   return (
-    <main id='educationPagea'>
+    <main id='educationPage'>
       <Education_Header />
-      <Education_Recent />
+      {/* <Education_Recent />
       <Education_FromScratch />
       <Education_MadeToOrder />
       <Education_ProjectFiles />
-      <Education_TutorialRequests />
+      <Education_TutorialRequests /> */}
     </main>
   )
 }
