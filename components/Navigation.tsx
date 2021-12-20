559e318d0b1c89d3eabef21b773f3ed48b79874f
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-19 18:06:54 -0600

    Changed nav's animation to trigger on #topOfPage instead of .lander

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
     }
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: '.lander',
+        trigger: '#topOfPage',
         start: 'top',
         end: 'top 50%',
         toggleActions: 'play none reverse none',
