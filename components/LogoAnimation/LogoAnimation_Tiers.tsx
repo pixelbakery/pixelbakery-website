6b713b189a8b2d089128f2f5c153fa434ac1db20
Jordan Lambrecht <jordan@pixelbakery.com>
2021-12-24 04:46:04 -0600

    expanded dynamic page header content

--- components/LogoAnimation/LogoAnimation_Tiers.tsx
+++ components/LogoAnimation/LogoAnimation_Tiers.tsx
@@ -55,6 +55,59 @@ function LogoAnimation_Tiers() {
               </ul>
             </div>
           </div>
+          {/* END ESSENTIALS */}
+
+          {/* ESSENTIALS */}
+          <div className='text-center border-2 rounded-xl border-pink bg-white px-3 py-6 transition transform duration-500 hover:scale-99'>
+            <div className='mb-4'>
+              <h3 className='text-2xl font-semibold mb-4 text-pink'>Essentials</h3>
+              <div className='text-4xl text-blue-dark font-extrabold'>$1600</div>
+            </div>
+            <div>
+              <ul className='unstyled text-wine text-lg w-fit mx-auto'>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  1 Concept
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  1 round of revisions
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ✅ Transition IN
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Transition OUT
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Passive Animation
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Instagram Pack
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Alternative Colors
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ White (Reverse)
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ .MOGRT Templates
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Lower Thirds
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Animated Transitions
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ Lower Thirds
+                </li>
+                <li className='my-6 text-left border-top border-bottom border-wine-300'>
+                  ❌ White (Reverse)
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* END ESSENTIALS */}
         </div>
       </div>
     </section>
--- components/PageHeader_VariableHeight.tsx
+++ components/PageHeader_VariableHeight.tsx
@@ -1,43 +1,114 @@
 import React from 'react'
 import Lottie from 'react-lottie-player'
-import Patterns_DarkBlue from '../data/patterns_darkblue.json'
+import classNames from 'classnames'
+import Patterns_Peach from '../data/Patterns_Peach.json'
+import Patterns_Yellow from '../data/Patterns_Yellow.json'
+import Patterns_Cream from '../data/Patterns_Cream.json'
+import Patterns_Wine from '../data/Patterns_Wine.json'
+import Patterns_Pink from '../data/Patterns_Pink.json'
+import Patterns_Pink_Light from '../data/Patterns_Pink_Light.json'
+import Patterns_Blue_Dark from '../data/Patterns_Blue_Dark.json'
+import Patterns_Blue from '../data/Patterns_Blue.json'
+import Patterns_Blue_Light from '../data/Patterns_Blue_Light.json'
 import PropTypes from 'prop-types'
-
 PageHeader_VariableHeight.propTypes = {
   header: PropTypes.string,
   subheader: PropTypes.string,
 }
 
+const playFrames = [
+  [0, 23],
+  [24, 95],
+]
+const PatternPeach = ({ pattern }) => {
+  return (
+    <Lottie
+      animationData={pattern}
+      loop
+      segments={playFrames}
+      play
+      rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
+      style={{ height: '100%' }}
+    />
+  )
+}
+
 function PageHeader_VariableHeight({ header, subheader }) {
-  const PatternBlue = () => {
-    return (
-      <Lottie
-        loop
-        animationData={Patterns_DarkBlue}
-        play
-        rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
-        style={{ height: '100%' }}
-      />
-    )
+  let setPattern = Math.floor(Math.random() * 3)
+
+  let headerClass = 'leading-none mb-0 pb-0 text-6xl lg:text-7xl'
+  let sectionClass = 'relative overflow-hidden lander-variableHeight my-4'
+  let textBoxClass =
+    'my-20 max-w-xs  sm:max-w-md md:max-w-lg lg:max-w-3xl py-12 flex justify-center '
+  let subheaderClass = 'my-2 text-2xl font-bold'
+  let pattern, color, accentColor, subheaderColor
+
+  if (setPattern === 0) {
+    pattern = Patterns_Peach
+    color = 'peach'
+    accentColor = 'pink-light'
+    subheaderColor = 'cream'
+  } else if (setPattern === 1) {
+    pattern = Patterns_Blue
+    color = 'blue'
+    accentColor = 'pink-light'
+    subheaderColor = 'blue-dark'
+  } else if (setPattern === 2) {
+    pattern = Patterns_Pink
+    color = 'pink'
+    accentColor = 'pink-light'
+    subheaderColor = 'blue-dark'
   }
+
+  // headerClass = classNames(headerClass, {
+  //   [`text-${accentColor}`]: true,
+  // })
+
+  // sectionClass = classNames(sectionClass, {
+  //   [`bg-${accentColor}`]: true,
+  // })
+  // textBoxClass = classNames(textBoxClass, {
+  //   [`bg-${color}`]: true,
+  // })
+  subheaderClass = classNames(subheaderClass, {
+    [`text-${subheaderColor}`]: true,
+  })
+
   return (
     <main>
       <section
-        className=' bg-blue  relative overflow-hidden lander-variableHeight my-4'
+        className={classNames(sectionClass, {
+          ['bg-blue']: pattern === Patterns_Pink,
+          ['bg-pink-light']: pattern === Patterns_Peach,
+          ['bg-yellow']: pattern === Patterns_Blue,
+        })}
         id='topOfPage'
       >
         <div
           className=' z-0 absolute  top-0 left-0 w-full h-full overflow-hidden lottie'
           id='lottie'
         >
-          <PatternBlue />
+          <PatternPeach pattern={pattern} />
         </div>
-
         <div className='relative py-16 my-20 h-full flex flex-col justify-center bg-transparent '>
-          <div className='bg-blue-dark my-20 max-w-xs  sm:max-w-md md:max-w-lg lg:max-w-3xl text-blue py-12 flex justify-center'>
+          <div
+            className={classNames(textBoxClass, {
+              'bg-pink': pattern === Patterns_Pink,
+              'bg-peach': pattern === Patterns_Peach,
+              'bg-blue': pattern === Patterns_Blue,
+            })}
+          >
             <div className='w-fit px-6'>
-              <h1 className='leading-none mb-0 pb-0 text-6xl lg:text-7xl'>{header}</h1>
-              <div className=' my-2 text-2xl font-bold  text-cream'>{subheader}</div>
+              <h1
+                className={classNames(headerClass, {
+                  ['text-blue']: pattern === Patterns_Pink,
+                  ['text-pink-light']: pattern === Patterns_Peach,
+                  ['text-yellow']: pattern === Patterns_Blue,
+                })}
+              >
+                {header}
+              </h1>
+              <div className={subheaderClass}>{subheader}</div>
             </div>
           </div>
         </div>
