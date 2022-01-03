49b87273c369cdd87186d4fb7658eb4006718af8
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-02 18:50:53 -0600

    removing all Projects code until it can be fixed

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -7,52 +7,24 @@ import Work_Awards from '../../components/Work/Work_Awards'
 import PageHeader_VariableHeight from '../../components/PageHeader/PageHeader_VarH'
 
 import Link from 'next/link'
-import { getAllProjects } from '../../lib/api_project'
-import Project from '../../types/project'
-type Props = {
-  projects: Project[]
-}
 
 // import Test from '../../components/Work/Test'
 
 // import Work_Portfolio from "../components/work-portfolio";
 // import Work_Filter from "../components/work-portfolio-filter";
-const Work = ({ projects }: Props) => {
-  console.log(projects)
-  projects.forEach((element) => {
-    console.log(element.slug)
-  })
+const Work = () => {
   return (
     <div>
       <PageHeader_VariableHeight header='What We Make' subheader='copy copy copy ' />
 
       {/* <Work_Services /> */}
       {/* <Test /> */}
-      <section>
-        {projects.map((project) => {
-          return (
-            <Link as={`/work/${project.slug}`} href='/work/[project]' key={project.slug} passHref>
-              <a>
-                {' '}
-                <h1>{project.slug}</h1>
-              </a>
-            </Link>
-          )
-        })}
-      </section>
+
       {/* <Work_Portfolio /> */}
       {/* <Work_Portfolio /> */}
-      {/* <Work_Client /> */}
-      {/* <Work_Awards /> */}
+      <Work_Client />
+      <Work_Awards />
     </div>
   )
 }
 export default Work
-
-export const getStaticProps = async () => {
-  const projects = getAllProjects(['slug', 'title', 'client'])
-
-  return {
-    props: { projects },
-  }
-}
