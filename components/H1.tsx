2fe16b457a7825e75cd14178f28f66e89b20b264
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-13 12:01:48 -0600

    Fixed naming convention bug

--- components/H1.tsx
+++ components/H1.tsx
@@ -7,11 +7,11 @@ type Props = {
   className?: string
 }
 
-const InnerWrapper = ({ color, children, ...className }: PropsWithChildren<Props>) => {
+const H1 = ({ color, children, ...className }: PropsWithChildren<Props>) => {
   return (
     <h2
       className={cn(
-        'font-bold max-w-md mt-0 text-6xl 2xl:text-8xl 2xl:max-w-4xl leading-none mb-12 2xl:mb-20',
+        'font-bold max-w-md mt-0 text-6xl 2xl:text-8xl 2xl:max-w-4xl leading-none mb-12 2xl:mb-20 mx-auto',
         [`text-${color}`],
         [`${className}`],
       )}
@@ -21,4 +21,4 @@ const InnerWrapper = ({ color, children, ...className }: PropsWithChildren<Props
   )
 }
 
-export default InnerWrapper
+export default H1
