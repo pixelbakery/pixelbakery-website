c119fe1dc36efd12623df8daa4d99ddb3f30bd5d
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-14 03:35:48 -0600

    global component enhancements

---
+++ components/Main.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode, PropsWithChildren } from 'react'
+import cn from 'classnames'
+
+type Props = {
+  children?: ReactNode
+  id?: string
+  className?: string
+}
+
+const Main = ({ children, id, className }: PropsWithChildren<Props>) => {
+  return (
+    <main id={id} className={cn('max-w-screen overflow-hidden', className)}>
+      {children}
+    </main>
+  )
+}
+
+export default Main
