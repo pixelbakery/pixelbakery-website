56a5bf99091d6b079575c621d38a7cb290291d94
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-14 03:35:09 -0600

    Contact Page progression

--- components/Contact/Contact_Jobs.tsx
+++ components/Contact/Contact_Jobs.tsx
@@ -1,45 +1,55 @@
 import React from 'react'
 import Button_Filled from '../parts/Button_Filled'
+import PageSection from '../PageSection'
+import H2 from '../H2'
+import InnerWrapper from '../InnerWrapper'
+import Lead from '../Lead'
 
 function Contact_Jobs() {
   return (
-    <section className='my-4 bg-wine mx-auto py-6'>
-      <div className='max-w-6xl mx-auto'>
-        <div className='flex flex-wrap'>
-          <h2>Looking for a job?</h2>
-          <div>
-            <h3>We&apos;re always searching for new family members</h3>
-            <p>
+    <PageSection className=' bg-wine '>
+      <InnerWrapper className=''>
+        <div className='grid grid-cols-1 lg:grid-cols-2 lg:gap-12'>
+          <div className='col-span-1'>
+            <H2 color='blue'> Looking for a job? </H2>
+          </div>
+          <div className='col-span-1'>
+            <Lead color='cream'>We&apos;re always searching for new family members</Lead>
+            <p className='text-cream font-medium mb-8 leading-tight text-lg'>
               Got what it takes to rise with us? Great! Send us a link to your résumé or portfolio
               to become part of our talent pool.
             </p>
           </div>
+
+          <div className=' lg:col-span-2 grid grid-cols-1 lg:grid-cols-3 lg:gap-4'>
+            <Button_Filled
+              center={true}
+              link={'/careers#positions'}
+              text={'get to work'}
+              textColor={'cream'}
+              bgColor={'blue'}
+              chevronDirection={'right'}
+            />
+            <Button_Filled
+              link={'/careers#vendors'}
+              center={true}
+              text={"get to freelancin'"}
+              textColor={'cream'}
+              bgColor={'blue'}
+              chevronDirection={'right'}
+            />
+            <Button_Filled
+              link={'/careers#talent'}
+              center={true}
+              text={"get to actin'"}
+              textColor={'cream'}
+              bgColor={'blue'}
+              chevronDirection={'right'}
+            />
+          </div>
         </div>
-      </div>
-      <div className='flex justify-center'>
-        <Button_Filled
-          link={'/'}
-          text={'get to work'}
-          textColor={'pink-light'}
-          bgColor={'blue'}
-          chevronDirection={'right'}
-        />
-        <Button_Filled
-          link={'/'}
-          text={"get to freelancin'"}
-          textColor={'pink-light'}
-          bgColor={'blue'}
-          chevronDirection={'right'}
-        />
-        <Button_Filled
-          link={'/'}
-          text={"get to actin'"}
-          textColor={'pink-light'}
-          bgColor={'blue'}
-          chevronDirection={'right'}
-        />
-      </div>
-    </section>
+      </InnerWrapper>
+    </PageSection>
   )
 }
 export default Contact_Jobs
