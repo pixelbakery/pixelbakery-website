015006baf5965999ef096e4f103300657950f4f9
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-17 13:52:01 -0600

    expanded case studies content

---
+++ pages/work/case-studies/oxbow-hay-the-oxbow-way.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Main from '../../../components/Main'
+
+// case study components
+import CaseStudies_Header from '../../../components/CaseStudies/CaseStudies_Header'
+import CaseStudies_Tags from '../../../components/CaseStudies/CaseStudies_Tags'
+import CaseStudies_OtherProjects from '../../../components/CaseStudies/CaseStudies_OtherProjects'
+import CaseStudies_BecomeAClient from '../../../components/CaseStudies/CaseStudies_BecomeAClient'
+import CaseStudies_Credits from '../../../components/CaseStudies/CaseStudies_Credits'
+import CaseStudies_PrevNext from '../../../components/CaseStudies/CaseStudies_PrevNext'
+
+// project info
+const title = 'Hay, The Oxbow Way'
+const tags = [{ name: 'Creative Concepting', url: '/' }]
+
+//client info
+const client = 'Oxbow'
+const clientLogo = '/img/client-logos/Black-Sola-Logo.png'
+const clientURL = 'https://oxbowanimalhealth.com'
+
+// Project Header
+const heroVideo = '511820753'
+const projectSubhead = "i'm a big old subheader"
+const projectExcerpt = 'copy copy copy'
+const projectIntroduction = 'copy copy copy'
+
+// credits
+const credits = [
+  { name: 'Jordan Lambrecht', title: 'Director' },
+  { name: 'Samee Callahan', title: 'Associate Creative Director' },
+]
+
+function caseStudy_Backcountry_GearheadMagic() {
+  return (
+    <Main>
+      <CaseStudies_Header
+        client={client}
+        clientURL={clientURL}
+        clientLogo={clientLogo}
+        projectName={title}
+        projectExcerpt={projectExcerpt}
+        projectIntroduction={projectIntroduction}
+        heroVideo={heroVideo}
+        projectSubhead={projectSubhead}
+      />
+      <CaseStudies_Tags tags={tags} />
+      <CaseStudies_Credits credits={credits} />
+      <CaseStudies_PrevNext title={title} />
+      <CaseStudies_OtherProjects client={client} title={title} />
+      <CaseStudies_BecomeAClient />
+    </Main>
+  )
+}
+
+export default caseStudy_Backcountry_GearheadMagic
---
+++ pages/work/project.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Post({ children, frontMatter }) {
+  const { title } = frontMatter
+  return (
+    <div>
+      <main>
+        <h1>{title}</h1>
+        <div>{children}</div>
+        <p>
+          <Link href='/'>
+            <a>Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
