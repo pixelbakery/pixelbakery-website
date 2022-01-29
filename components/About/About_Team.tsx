0f99b4f03778a07c3e174a6c2f6e1f030de438fb
Jordan Lambrecht <jordan@pixelbakery.co>
2022-01-29 03:27:13 -0600

    About page progress

--- components/About/About_Team.tsx
+++ components/About/About_Team.tsx
@@ -9,28 +9,31 @@ type Props = {
 }
 
 function About_Team({ people }) {
-  const headerIndex = 2
-  const leadIndex1 = (people.length * -1 - 5) * 1
-  const leadIndex2 = (people.length - 2) * 1
   function Header() {
     return (
-      <div className='relative w-full  aspect-w-3 aspect-h-4 bg-blue flex flex-col justify-center px-8'>
-        <H2 color='blue-dark'>Who we are</H2>
+      <div className='relative w-full  col-span-1 bg-blue flex flex-col justify-center px-8'>
+        <H2 color='blue-dark' className='my-0 py-0 mb-0'>
+          Who we are
+        </H2>
       </div>
     )
   }
   function Leader1() {
     return (
-      <div className='relative w-full bg-blue-dark col-span-1 flex flex-col justify-center px-8'>
-        <Lead color='blue' className='pt-4'>
+      <div className='relative w-full bg-pink col-span-2  aspect-1/1 md:aspect-3/4 md:col-span-1  flex flex-col justify-center px-10'>
+        <Lead color='blue-dark' className='pt-4'>
           we&apos;re a ragtag team of scrappy designers, animators, and producers
+        </Lead>
+        <Lead color='peach' className='pt-4 md:hidden'>
+          we create intentional and beautiful moments for exciting people that share our beliefs and
+          passions
         </Lead>
       </div>
     )
   }
   function Leader2() {
     return (
-      <div className='relative w-full bg-blue-dark col-span-1 flex flex-col justify-center px-8'>
+      <div className='hidden md:flex relative w-full bg-blue-dark col-span-1 aspect-h-4 md:flex-col justify-center px-8'>
         <Lead color='blue' className='mb-0 pb-0'>
           we create intentional and beautiful moments for exciting people that share our beliefs and
           passions
@@ -40,13 +43,20 @@ function About_Team({ people }) {
   }
 
   const shifted = people
+  const headerIndex = 0
+
   shifted.splice(headerIndex, 0, <Header />)
+  const leadIndex1 = (shifted.length - 7) * 1
+  console.log(leadIndex1, ' – ', shifted.length)
   shifted.splice(leadIndex1, 0, <Leader1 />)
+
+  const leadIndex2 = (shifted.length - 2) * 1
+  console.log(leadIndex2)
   shifted.splice(leadIndex2, 0, <Leader2 />)
-  console.log(shifted)
+
   return (
     <section className='my-4' id='team'>
-      <div className='grid gap-4 grid-cols-2 md:grid-cols-3 2xl:grid-cols-4'>
+      <div className='grid gap-1 -4 grid-cols-2 md:grid-cols-3 2xl:grid-cols-4'>
         {shifted.map((person, index) => {
           if (index === headerIndex) {
             return <Header />
