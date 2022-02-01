20e0c665edee011946ba1e65747132c1c7b8adfb
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-01 03:01:06 -0600

    Makin' bio pages, fixed some social link issues

--- types/socials.ts
+++ types/socials.ts
@@ -1,13 +1,13 @@
 import { Url } from 'url'
 
-type Socials = {
-  instagram: Url
-  linkedIn: Url
-  facebook: Url
-  spotify: Url
-  twitter: Url
-  personalWebsite: Url
-  etsy: Url
-}
+type Socials = [
+  instagram: Url,
+  linkedin: Url,
+  facebook: Url,
+  spotify: Url,
+  twitter: Url,
+  personalWebsite: Url,
+  etsy: Url,
+]
 
 export default Socials
