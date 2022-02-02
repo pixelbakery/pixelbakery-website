8c73c400e53ae0154973823d1f3803933ecb877a
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-02 01:25:25 -0600

    EOD, More work on team bios

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -28,7 +28,15 @@ function About({ allPeople }: Props) {
 
 export default About
 export const getStaticProps = async () => {
-  const allPeople = getAllPeople(['slug', 'socials', 'name', 'active', 'title', 'photos'])
+  const allPeople = getAllPeople([
+    'slug',
+    'socials',
+    'details',
+    'name',
+    'active',
+    'title',
+    'photos',
+  ])
 
   return {
     props: { allPeople },
