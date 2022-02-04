e457d5dc756324edb9aa737f8aebe446ec545bfb
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-04 17:18:18 -0600

    Added instagram feed to footer, resolves #64

--- components/foot/Footer_Main.tsx
+++ components/foot/Footer_Main.tsx
@@ -10,6 +10,8 @@ import footerMenu from '@data/footerMenu'
 import footerMenuMore from '@data/footerMenuMore'
 import { useEffect, useState } from 'react'
 import gsap from 'gsap'
+import { InstagramGallery } from '@parts/Instagram'
+
 export const Footer: NextPage = () => {
   const [testModalOpen, setTestModal] = useState(false)
   // const [selectedStar, setSelectedStar] = useState()
@@ -30,129 +32,134 @@ export const Footer: NextPage = () => {
   // }
   const year = new Date().getFullYear()
   return (
-    <footer
-      className='relative bg-peach px-6 lg:px-12 py-6 lg:py-24 lg:max-h-screen mb-4'
-      id='footer'
-    >
-      <section
-        id='croissantModal'
-        className='transform-gpu will-change-transform absolute w-full h-full top-0 left-0 bg-peach z-30'
+    <div>
+      <footer
+        className='relative bg-peach px-6 lg:px-12 py-6 lg:py-24 lg:max-h-screen mb-4'
+        id='footer'
       >
-        <Footer_Mailchimp2 onModalUpdate={updateModal} />
-      </section>
+        <section
+          id='croissantModal'
+          className='transform-gpu will-change-transform absolute w-full h-full top-0 left-0 bg-peach z-30'
+        >
+          <Footer_Mailchimp2 onModalUpdate={updateModal} />
+        </section>
 
-      <div className='mx-auto max-w-6xl'>
-        <div className='grid grid-cols-1 lg:grid-cols-3 lg:mb-12 gap-8 h-full'>
-          <div className='h-full flex flex-col justify-between'>
-            <div className='flex justify-center lg:justify-start'>
-              <Logo_Type />
-            </div>
+        <div className='mx-auto max-w-6xl'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 lg:mb-12 gap-8 h-full'>
+            <div className='h-full flex flex-col justify-between'>
+              <div className='flex justify-center lg:justify-start'>
+                <Logo_Type />
+              </div>
 
-            <nav className=' pt-5 mt-3'>
-              <ul className='grid grid-cols-2 lg:grid-cols-1  text-left text-xl lg:text-4xl font-semibold gap-x-2 lg:font-black'>
-                {footerMenu.map((navitem) => (
-                  <Footer_Nav navitem={navitem} key={navitem.text} />
-                ))}
-              </ul>
-            </nav>
-          </div>
-
-          <div className='flex flex-col justify-between h-full md:pr-6'>
-            <div className='self-center w-full hidden lg:block'>
-              <div className='mx-auto border-b-4 border-cream text-bold max-w-xs  md:pr-4  md:mr-4'>
-                <h3 className='text-center md:text-left font-extrabold text-3xl text-cream'>
-                  studio
-                </h3>
-              </div>
-              <div>
-                <ul className='mt-3 text-center md:text-left'>
-                  <li>
-                    <a
-                      className='text-cream text-xl'
-                      target='_blank'
-                      href='https://g.page/pixelbakery?share'
-                    >
-                      2124 y st ste 208
-                      <br />
-                      lincoln, ne 68503
-                    </a>
-                  </li>
-                  <li className='mt-3 text-xl text-cream'>
-                    <a href='tel:4024138366' target='_blank' rel='noreferrer'>
-                      402 413 8366
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className='lg:pt-4 md:pr-4'>
-              <div className='hidden lg:block mx-auto lg:mt-4 border-b-4 border-cream mb-4 max-w-xs md:pr-4'>
-                <h3 className='text-center md:text-left font-extrabold text-3xl  leading-none mt-0 text-cream'>
-                  more stuff
-                </h3>
-              </div>
-              <nav className='flex justify-center'>
-                <ul className='mt-3 grid grid-cols-2 lg:grid-cols-1 w-fit align-center gap-y-2 gap-x-3'>
-                  {footerMenuMore.map((navitem) => (
-                    <Footer_Nav_SubNav navitem={navitem} key={navitem.text} />
+              <nav className=' pt-5 mt-3'>
+                <ul className='grid grid-cols-2 lg:grid-cols-1  text-left text-xl lg:text-4xl font-semibold gap-x-2 lg:font-black'>
+                  {footerMenu.map((navitem) => (
+                    <Footer_Nav navitem={navitem} key={navitem.text} />
                   ))}
                 </ul>
               </nav>
             </div>
-          </div>
 
-          <div className='flex flex-col justify-between h-full'>
-            <div className='text-center lg:mb-4'>
-              <Footer_HappyCard />
-            </div>
-            <div className='w-full lg:py-4 lg:my-3'>
-              <div className='w-full' id='footer-social'>
-                <Footer_SocialLinks />
+            <div className='flex flex-col justify-between h-full md:pr-6'>
+              <div className='self-center w-full hidden lg:block'>
+                <div className='mx-auto border-b-4 border-cream text-bold max-w-xs  md:pr-4  md:mr-4'>
+                  <h3 className='text-center md:text-left font-extrabold text-3xl text-cream'>
+                    studio
+                  </h3>
+                </div>
+                <div>
+                  <ul className='mt-3 text-center md:text-left'>
+                    <li>
+                      <a
+                        className='text-cream text-xl'
+                        target='_blank'
+                        href='https://g.page/pixelbakery?share'
+                      >
+                        2124 y st ste 208
+                        <br />
+                        lincoln, ne 68503
+                      </a>
+                    </li>
+                    <li className='mt-3 text-xl text-cream'>
+                      <a href='tel:4024138366' target='_blank' rel='noreferrer'>
+                        402 413 8366
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-              {/* <div className='w-full flex justify-center'>
-                <span className='text-cream italic font-bold text-center text-lg'>
-                  fresh puns and nice buns
-                </span>
-              </div> */}
+              <div className='lg:pt-4 md:pr-4'>
+                <div className='hidden lg:block mx-auto lg:mt-4 border-b-4 border-cream mb-4 max-w-xs md:pr-4'>
+                  <h3 className='text-center md:text-left font-extrabold text-3xl  leading-none mt-0 text-cream'>
+                    more stuff
+                  </h3>
+                </div>
+                <nav className='flex justify-center'>
+                  <ul className='mt-3 grid grid-cols-2 lg:grid-cols-1 w-fit align-center gap-y-2 gap-x-3'>
+                    {footerMenuMore.map((navitem) => (
+                      <Footer_Nav_SubNav navitem={navitem} key={navitem.text} />
+                    ))}
+                  </ul>
+                </nav>
+              </div>
             </div>
-            <div className='hidden lg:flex justify-center w-full my-3'>
-              <Button_Filled
-                center={true}
-                text={'Start something wonderful'}
-                link={'/onboarding'}
-                bgColor={'blue-dark'}
-                textColor={'cream'}
-                chevronDirection='right'
-              />
-            </div>
-            <div className='hidden lg:block text-center lg:mt-3'>
-              <a
-                className='text-cream  text-2xl font-extrabold'
-                href='mailto:hello@pixelbakery.com'
-                target='_blank'
-                rel='noreferrer'
-              >
-                hello@pixelbakery.com
-              </a>
+
+            <div className='flex flex-col justify-between h-full'>
+              <div className='text-center lg:mb-4'>
+                <Footer_HappyCard />
+              </div>
+              <div className='w-full lg:py-4 lg:my-3'>
+                <div className='w-full' id='footer-social'>
+                  <Footer_SocialLinks />
+                </div>
+                {/* <div className='w-full flex justify-center'>
+                  <span className='text-cream italic font-bold text-center text-lg'>
+                    fresh puns and nice buns
+                  </span>
+                </div> */}
+              </div>
+              <div className='hidden lg:flex justify-center w-full my-3'>
+                <Button_Filled
+                  center={true}
+                  text={'Start something wonderful'}
+                  link={'/onboarding'}
+                  bgColor={'blue-dark'}
+                  textColor={'cream'}
+                  chevronDirection='right'
+                />
+              </div>
+              <div className='hidden lg:block text-center lg:mt-3'>
+                <a
+                  className='text-cream  text-2xl font-extrabold'
+                  href='mailto:hello@pixelbakery.com'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  hello@pixelbakery.com
+                </a>
+              </div>
             </div>
           </div>
+          <div className='flex flex-col md:flex-row justify-center md:justify-between'>
+            <button
+              onClick={updateModal}
+              className='self-center max-w-xs group text-2xl ease-in-out bg-cream px-3 rounded-md py-2 duration-300 hover:scale-98 drop-shadow-md hover:drop-shadow-sm '
+            >
+              <i className='block duration-300 ease-in-out group-hover:rotate-360'> 🥐</i>
+            </button>
+            <span className='self-center text-center mt-4 md:mt-0 italic text-sm  text-cream'>
+              © {year} Pixel Bakery Design Studio
+              <br />
+              Made with lust and flour in Lincoln, Nebraska & NW Arkansas
+            </span>
+            <div className='hidden md:block' />
+          </div>
         </div>
-        <div className='flex flex-col md:flex-row justify-center md:justify-between'>
-          <button
-            onClick={updateModal}
-            className='self-center max-w-xs group text-2xl ease-in-out bg-cream px-3 rounded-md py-2 duration-300 hover:scale-98 drop-shadow-md hover:drop-shadow-sm '
-          >
-            <i className='block duration-300 ease-in-out group-hover:rotate-360'> 🥐</i>
-          </button>
-          <span className='self-center text-center mt-4 md:mt-0 italic text-sm  text-cream'>
-            © {year} Pixel Bakery Design Studio
-            <br />
-            Made with lust and flour in Lincoln, Nebraska & NW Arkansas
-          </span>
-          <div className='hidden md:block' />
-        </div>
+      </footer>
+      <div className='my-4'>
+        <InstagramGallery count={14} />
       </div>
-    </footer>
+    </div>
   )
 }
 
---
+++ components/parts/Instagram.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import cn from 'classnames'
+interface GalleryProps {
+  count: number
+}
+
+export const InstagramGallery = (props: GalleryProps) => {
+  const [loading, setLoading] = useState<Boolean>(true)
+  const [error, setError] = useState<Boolean>(false)
+  const [instagramData, setInstagramData] = useState<any>(null)
+  const accessToken =
+    'IGQVJXUmVjdC11cTl4VUY1YWpzcmFMVUR2OVpZAMVZAiMGVOWVVxRXVXLXFYMnduZAXpsT0EyUGNacl96dTJYSlRnd2FlWUFjbkNBN1dhVzRRamcya2k2MmJMQlowVVJVNzJDTG9zMWF2RC0yeklDZAE4yOAZDZD'
+  const fetchInstagramData = (url: string) => {
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        // console.log({ data })
+        if (data.hasOwnProperty('error')) {
+          setLoading(false)
+          setError(true)
+        } else {
+          setInstagramData(data)
+          setLoading(false)
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+        setError(true)
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    const url = `https://graph.instagram.com/me/media?fields=media_count,media_type,permalink,media_url,caption&limit=${props.count}&access_token=${accessToken}`
+    fetchInstagramData(url)
+  }, [])
+
+  if (loading) {
+    return <div className='instagram-gallery'>LOADING...</div>
+  }
+
+  if (error) {
+    return (
+      <div className='instagram-gallery'>
+        {' '}
+        Something went terribly wrong - can't load instagram feed.
+      </div>
+    )
+  }
+
+  return (
+    <div className='grid grid-cols-4 lg:grid-cols-6 2xl:grid-cols-7 gap-2 instagram-gallery'>
+      {instagramData.data.slice(0, props.count).map((item: any, index: any) => (
+        <div
+          key={index}
+          className={cn('bg-yellow col-span-1 instagram-item aspect-w-1 aspect-h-1', {
+            ['hidden lg:block 2xl:block']: index > 6 && index < 11,
+            ['hidden  2xl:block']: index >= 11 && index < 13,
+          })}
+        >
+          <a
+            key={index}
+            href={item.permalink}
+            className='instagram-link'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {item.media_type === 'IMAGE' || item.media_type === 'CAROUSEL_ALBUM' ? (
+              <div className=' w-full h-full'>
+                <Image
+                  layout='fill'
+                  objectFit='cover'
+                  className='instagram-image'
+                  key={index}
+                  src={item.media_url}
+                  alt={item.caption}
+                  quality={33}
+                />
+              </div>
+            ) : (
+              <video
+                className='instagram-image object-cover object-center w-full h-full'
+                autoPlay
+                muted
+                loop
+                playsInline
+                key={index}
+              >
+                <source src={item.media_url} type='video/mp4' />
+              </video>
+            )}
+          </a>
+        </div>
+      ))}
+    </div>
+  )
+}
