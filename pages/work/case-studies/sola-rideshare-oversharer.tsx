023a3cf1317beb831a82c6d98965e9d386fbbff2
Maddie Hahn <maddie@pixelbakery.com>
2022-02-15 12:44:07 -0600

    added 'process' and logo

--- pages/work/case-studies/sola-rideshare-oversharer.tsx
+++ pages/work/case-studies/sola-rideshare-oversharer.tsx
@@ -1,20 +1,23 @@
 import Main from '@parts/Main'
-
+import { PropsWithChildren } from 'react'
 // case study components
 import CaseStudies_Header from '@caseStudies/CaseStudies_Header'
 import CaseStudies_Tags from '@caseStudies/CaseStudies_Tags'
+import CaseStudies_Credits from '@caseStudies/CaseStudies_Credits'
+
 import CaseStudies_OtherProjects from '@caseStudies/CaseStudies_OtherProjects'
 import CaseStudies_BecomeAClient from '@caseStudies/CaseStudies_BecomeAClient'
-import CaseStudies_Credits from '@caseStudies/CaseStudies_Credits'
 import CaseStudies_PrevNext from '@caseStudies/CaseStudies_PrevNext'
+import CaseStudies_Description from '@caseStudies/CaseStudies_Description'
 
+import Image from 'next/image'
 // project info
 const title = 'Rideshare Oversharer'
 const tags = [{ name: 'Creative Concepting', url: '/' }]
 
 //client info
 const client = 'SOLA'
-const clientLogo = '/img/client-logos/Black-Sola-Logo.png'
+const clientLogo = '/img/clients/pixelbakery_client_sola.png'
 const clientURL = 'https://sola.com'
 
 // Project Header
@@ -51,8 +54,40 @@ function caseStudy_Backcountry_GearheadMagic() {
         projectSubhead={projectSubhead}
       />
       <CaseStudies_Tags tags={tags} />
+      <CaseStudies_Tags tags={tags} />
+      <CaseStudies_Description
+        headerColor={'dark-blue'}
+        textColor={'cream'}
+        bgColor={'blue'}
+        header={'Process'}
+      >
+         <p>
+         To bring an awkward Uber ride to life, you’ve gotta film in a moving car. Not the easiest task. Luckily, our small but mighty team works well in tight spaces. We brought in a few reinforcements for an extra hand with lighting, filming, makeup, and assistance on set. We lucked out and found two local actors perfect for the Uber driver and his unfortunate passenger, and both were ready and willing to drive around in circles for hours and brave the bitter, midwest winter. We’ve got to give extra props to Kabin (the Driver) for his execution of the script, but also being able to ad-lib beautifully when we needed him to. Much of what made the final cut was made up on the spot.
+        </p>
+        <p>
+        Authenticity was key with this project, and we felt a green screen just wouldn’t do the scene justice. Instead, we were able to mount a Black Magic Pocket Cinema 4k on the hood of the car to get shots through the windshield, and then inside the car for some additional angles. We directed via walkie talkies and followed in a car. Many people were crammed in trunks. Our mothers disapproved. Exciting stuff!
+        </p>
+        <p>
+        Authenticity was key with this project, and we felt a green screen just wouldn’t do the scene justice. Instead, we were able to mount a Black Magic Pocket Cinema 4k on the hood of the car to get shots through the windshield, and then inside the car for some additional angles. We directed via walkie talkies and followed in a car. Many people were crammed in trunks. Our mothers disapproved. Exciting stuff!
+        </p>
+
+      </CaseStudies_Description>
+
+     <CaseStudies_Description header={''} bgColor={'cream'}>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <div className='col-span-1 relative h-full w-full'>
+            <Image
+              src={'/img/work/KN_ECommPromo_SnackPack.gif'}
+              layout={'fill'}
+              alt={'DESCRIPTION GOES HERE'}
+              objectFit={'contain'}
+            />
+          </div>
+        </div>
+      </CaseStudies_Description>
       <CaseStudies_Credits credits={credits} />
       <CaseStudies_PrevNext title={title} />
+
       <CaseStudies_OtherProjects client={client} title={title} />
       <CaseStudies_BecomeAClient />
     </Main>
