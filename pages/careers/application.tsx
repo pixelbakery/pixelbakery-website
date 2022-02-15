33034d929b1be7455581feacbfa27931ca3108a8
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-15 01:39:20 -0600

    Resolves #67, Support for dynamic job posts (mdx)

--- pages/careers/application.tsx
+++ pages/careers/application.tsx
@@ -1,4 +1,16 @@
+import PageHeader_VarH from '@pageHeaders/PageHeader_VarH'
+import InnerWrapper from '@parts/InnerWrapper'
+import Main from '@parts/Main'
+import PageSection from '@parts/PageSection'
+
 function Careers_Application() {
-  return <>Application Page</>
+  return (
+    <Main>
+      <PageHeader_VarH header='Application' subheader='get at it' />
+      <PageSection>
+        <InnerWrapper></InnerWrapper>
+      </PageSection>
+    </Main>
+  )
 }
 export default Careers_Application
