b665efdfdd9725a9f288ad52aff0c1f93795b6e5
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-18 13:25:12 -0600

    Code Cleanup

--- components/pageSpecific/logoAnimation/LogoAnimation_GetStarted.tsx
+++ components/pageSpecific/logoAnimation/LogoAnimation_GetStarted.tsx
@@ -3,16 +3,17 @@ import H2 from '@typography/H2'
 
 import LogoAnimation_GetStarted_Form from './LogoAnimation_GetStarted_Form'
 import Button_Filled from '@parts/Button_Filled'
+import PageSection from '@parts/PageSection'
 
 function LogoAnimation_GetStarted() {
   return (
-    <section className='my-4 px-12 py-12 bg-white' id='descriptions'>
+    <PageSection className='my-4 px-12 py-12 bg-white' id='descriptions'>
       <InnerWrapper>
         <H2 className='pb-6'>Get Started</H2>
 
         <LogoAnimation_GetStarted_Form />
       </InnerWrapper>
-    </section>
+    </PageSection>
   )
 }
 export default LogoAnimation_GetStarted
