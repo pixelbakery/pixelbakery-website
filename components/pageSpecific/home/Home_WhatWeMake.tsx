dea2af0c9d9cb9c1aed38b8d621b6c76e04f2d66
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-24 16:05:47 -0600

    copy updates

--- components/pageSpecific/home/Home_WhatWeMake.tsx
+++ components/pageSpecific/home/Home_WhatWeMake.tsx
@@ -1,25 +1,29 @@
 import InnerWrapper from '@parts/InnerWrapper'
 import PageSection from '@parts/PageSection'
 import H2 from '@typography/H2'
+import Lead from '@typography/Lead'
 import Vimeo from '@u-wave/react-vimeo'
 
 function Home_WhatWeMake() {
   return (
     <PageSection>
       <InnerWrapper>
-        <div className='text-pink text-2xl  2xl:text-4xl  leading-noone font-bold'>
-          to put it simply,
-        </div>
+        <div className='grid grid-cols-1 xl:grid-cols-2 xl:gap-x-16 mb-24'>
+          <div>
+            <div className='text-pink text-2xl  2xl:text-4xl  leading-noone font-bold'>
+              to put it simply,
+            </div>
 
-        <H2 color='blue' className='text-pb-blue-dark 2xl:text-8xl mt-0 pt-0 max-w-4xl'>
-          We build beautifully immersive worlds{' '}
-        </H2>
-        <div className='mx-auto lg:mx-0 lg:w-3/5  flex flex-col  justify-center py-8 mb-4'>
-          <p className='lead  text-wine max-w-xl'>
-            Dynamically reconceptualize business schemas whereas ethical meta-services.
-            Monotonectally develop premier synergy and resource maximizing value. Competently
-            redefine extensive information.
-          </p>
+            <H2 color='blue' className='text-pb-blue-dark 2xl:text-8xl mt-0 pt-0 max-w-4xl'>
+              We build beautifully immersive worlds{' '}
+            </H2>
+          </div>
+          <div className='mx-auto lg:mx-0 w-full flex flex-col  justify-center lg:py-8 mb-4'>
+            <Lead color='peach' className='lg:mb-4'>
+              Our team creates intentional, high-quality products that are results of purposeful
+              visions and not taking any shortcuts.
+            </Lead>
+          </div>
         </div>
         <div className=' w-full'>
           <Vimeo video={'523920082'} responsive={true} controls={true} />
