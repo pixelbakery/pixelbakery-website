48f2a76525d2249de2b6371cbeec999478927fc2
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-26 13:13:35 -0600

    Closes #161, closes #186, closes #185

--- pages/services/animation.tsx
+++ pages/services/animation.tsx
@@ -3,20 +3,20 @@ import Services_Subservices from '@services/Services_Subservices'
 import Services_Related from '@services/Services_Related'
 import Services_AllServices from '@services/Services_AllServices'
 import Services_OurProcess from '@services/Services_OurProcess'
+import Lead from '@typography/Lead'
 
 export default function Services() {
   return (
     <div>
       <Services_Header serviceName='Animation'>
-        <p className='text-peach font-extrabold text-lg my-3 leading-relaxed'>
-          Proactively repurpose premier leadership via sticky architectures. Uniquely predominate
-          distinctive convergence for stand-alone alignments.{' '}
-        </p>
+        <Lead className='text-peach font-extrabold text-lg my-3 leading-relaxed'>
+          We specialize in 2D and 3D animation, motion graphics, stop motion, and 3D modeling
+        </Lead>
         <p className='mt-6 leading-loose'>
-          Phosfluorescently restore enabled models before premium solutions. Monotonectally initiate
-          cross-platform strategic theme areas vis-a-vis out-of-the-box testing procedures.
-          Compellingly deploy high-payoff catalysts for change via empowered solutions. Proactively
-          empower.
+          Every animation project is full-service and includes project management, creative
+          concepting, sound design, storyboarding, and voiceover as needed. Whether you’re looking
+          for a lower thirds animation template or a sizzle reel of your company’s coolest products,
+          we’ve got you covered. Check out what else we have to offer below.
         </p>
       </Services_Header>
       <Services_Subservices />
