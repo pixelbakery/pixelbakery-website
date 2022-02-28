2bcb8772d4bc615af75f66c810f9f4ac93127133
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-28 09:57:31 -0600

    Overhauling services pages, resolves #187

--- components/pageSpecific/services/Services_Header.tsx
+++ components/pageSpecific/services/Services_Header.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, PropsWithChildren } from 'react'
 import cn from 'classnames'
+import PageSection from '@parts/PageSection'
+import H1 from '@typography/H1'
 
 type Props = {
   children?: ReactNode
@@ -10,22 +12,30 @@ type Props = {
 
 function Services_Header({ serviceName, children, className }: PropsWithChildren<Props>) {
   return (
-    <section id='services-lander' className='w-full lander grid grid-cols-2 mt-4 mb-4 '>
-      <div className='bg-peach w-full'>
-        <div className='relative w-full h-full overflow-hidden'>
-          <video muted autoPlay loop preload='true' className='absolute object-cover w-full h-full'>
-            <source src='/img/Pixel-Bakery_3D-Oven-Render.mp4' />
-          </video>
+    <PageSection>
+      <div className='w-full  grid gap-y-16 grid-cols-2'>
+        <div className='bg-peach aspect-3/4  w-full col-span-2'>
+          <div className='relative w-full h-full overflow-hidden'>
+            <video
+              muted
+              autoPlay
+              loop
+              preload='true'
+              className='absolute object-cover w-full h-full'
+            >
+              <source src='/img/Pixel-Bakery_3D-Oven-Render.mp4' />
+            </video>
+          </div>
+        </div>
+        <div className='bg-egg col-span-2'>
+          <div className=' max-w-md h-full flex flex-col mx-auto justify-center'>
+            <span className='mt-0 -mb-2 py-0 text-blue font-extrabold text-lg'>Services</span>
+            <H1 className='mt-0 pt-0 mb-3 text-blue-dark'>{serviceName}</H1>
+            {children}
+          </div>
         </div>
       </div>
-      <div className='bg-egg'>
-        <div className=' max-w-md h-full flex flex-col mx-auto justify-center'>
-          <span className='mt-0 -mb-2 py-0 text-blue font-extrabold text-lg'>Services</span>
-          <h1 className='mt-0 pt-0 mb-3 text-blue-dark'>{serviceName}</h1>
-          {children}
-        </div>
-      </div>
-    </section>
+    </PageSection>
   )
 }
 export default Services_Header
