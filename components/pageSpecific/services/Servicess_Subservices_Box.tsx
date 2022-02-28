a7105d346ad31a9bb3fc2084c06b9dd5e504dded
Jordan Lambrecht <jordan@pixelbakery.co>
2022-02-28 09:57:31 -0600

    Overhauling services pages

--- components/pageSpecific/services/Servicess_Subservices_Box.tsx
+++ components/pageSpecific/services/Servicess_Subservices_Box.tsx
@@ -1,14 +1,14 @@
 function Services_Subservices_Box({ subservice }) {
   return (
-    <div className='my-12 border-4 border-blue rounded-lg flex py-6 px-4'>
-      <div className='text-center px-6 flex flex-col justify-center'>
+    <article className='my-12 border-4 border-blue rounded-lg   w-full py-6 px-4'>
+      <div className='text-center px-6 flex flex-row md:flex-col justify-center'>
         <span className=' text-6xl'>{subservice.icon}</span>
       </div>
       <div className='px-4'>
         <h3 className='text-peach text-3xl font-semibold'>{subservice.service}</h3>
         <p className='opacity-90 text-wine'>{subservice.desc}</p>
       </div>
-    </div>
+    </article>
   )
 }
 export default Services_Subservices_Box
