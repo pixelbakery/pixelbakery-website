f37f48735ac80a2ecd033cab9d38eff8d95c700b
Jordan Lambrecht <jordan@pixelbakery.co>
2022-03-04 00:08:25 -0600

    Fixed #184

--- components/pageSpecific/about/About_Awards.tsx
+++ components/pageSpecific/about/About_Awards.tsx
@@ -4,42 +4,23 @@ import awards from '@data/awards'
 import H2 from '@typography/H2'
 
 function About_Awards() {
-  // useEffect(() => {
-  //   const awards = gsap.timeline()
-  //   awards.from('.awardslist .award', {
-  //     overwrite: 'auto',
-  //     ease: 'power3.out',
-  //     stagger: 0.08,
-  //     opacity: 0,
-  //     y: -15,
-  //   })
-  //   ScrollTrigger.create({
-  //     trigger: '#awards',
-  //     toggleActions: 'play reverse play reverse',
-  //     start: 'top center',
-  //     end: 'bottom center',
-
-  //     animation: awards,
-  //   })
-  // }, [])
-
   return (
     <PageSection color={'blue'} id='awards'>
       <InnerWrapper>
-        <div className=' grid grid-cols-1 xl:grid-cols-3 gap-12 xl:gap-32'>
+        <div className=' grid grid-cols-1 xl:grid-cols-3 gap-2 md:gap-12 xl:gap-32'>
           <div className='col-span-1'>
             <H2 color='blue-dark'>
               awards and recognition<span className='hidden lg:inline'>*</span>
             </H2>
             <div className='-mt-8 text-2xl text-cream font-extrabold'>swoosh.</div>
-            <p className='hidden lg:block max-w-lg mt-6 text-xs text-cream font-medium'>
+            <p className='mb-0 pb-0 max-w-lg mt-6 text-xs text-cream font-medium'>
               *awards are stupid and meaningless, but idk, i guess everything is. might as well
               smoke &apos;em if you got &apos;em, right?
             </p>
           </div>
           <div className='col-span-2  flex flex-col justify-between mx-auto w-full '>
             <ul
-              className='w-fit self-end text-cream font-body text-xs sm:text-sm md:text-lg font-medium md:font-bold  relative awardsList'
+              className='w-fit self-end text-cream font-body text-sm sm:text-sm md:text-lg font-medium md:font-bold  relative awardsList'
               id='awardsList1'
             >
               {awards.map((award, index) => {
