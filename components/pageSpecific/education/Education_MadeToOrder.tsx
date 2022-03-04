2a413b12e5bc9dd4b1b1e6082a0bf2d035de5182
Jordan Lambrecht <jordan@pixelbakery.co>
2022-03-04 00:00:05 -0600

    Closed #117, Closed #116

--- components/pageSpecific/education/Education_MadeToOrder.tsx
+++ components/pageSpecific/education/Education_MadeToOrder.tsx
@@ -2,6 +2,7 @@ import H2 from '@typography/H2'
 
 import InnerWrapper from '@parts/InnerWrapper'
 import PageSection from '@parts/PageSection'
+import Lead from '@typography/Lead'
 
 function Education_MadeToOrder() {
   return (
@@ -11,10 +12,9 @@ function Education_MadeToOrder() {
           Made To Order
         </H2>
 
-        <p className='max-w-md text-xl text-blue-dark leading-none my-6'>
-          Dynamically deploy B2B customer service before next-generation niches. Synergistically
-          target standards compliant niches and client-centric.
-        </p>
+        <Lead className='max-w-md text-xl text-blue-dark leading-none my-6'>
+          One-off tutorials on specific somethings.
+        </Lead>
         <p className='font-extrabold text-wine-100 italic text-4xl '>coming soon...</p>
       </InnerWrapper>
     </PageSection>
