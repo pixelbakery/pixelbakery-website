6fc27c2617ba29451083bdf2408e40e3077b3f6a
Jordan Lambrecht <jordan@pixelbakery.co>
2022-03-27 16:47:54 -0500

    Added max-width

--- components/parts/VimeoPlayer.tsx
+++ components/parts/VimeoPlayer.tsx
@@ -2,7 +2,7 @@ import Vimeo from '@u-wave/react-vimeo'
 
 function Video({ url, ...props }) {
   return (
-    <div className='markdown-video aspect-16/9 my-4'>
+    <div className='markdown-video aspect-16/9 my-4 xl:my-8 max-w-6xl mx-auto'>
       <Vimeo
         video={props.videoID as string}
         autoplay={false}
