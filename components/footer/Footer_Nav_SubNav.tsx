cf904b0acda646d6091b63b59bd454280313655b
Jordan Lambrecht <jordan@pixelbakery.co>
2022-03-30 14:12:41 -0500

    Responsive styling changes 💅

--- components/footer/Footer_Nav_SubNav.tsx
+++ components/footer/Footer_Nav_SubNav.tsx
@@ -3,7 +3,9 @@ export default function FooterNavMoreCard({ navitem }) {
   return (
     <li className='text-left leading-none text-cream text-md cursor-pointer my-0 py-0'>
       <Link href={navitem.url} passHref>
-        <a href={navitem.url}>{navitem.text}</a>
+        <a className='my-0 py-0 leading-none' href={navitem.url}>
+          {navitem.text}
+        </a>
       </Link>
     </li>
   )
