e356ca9bbf77977b5f126c9d022eb3cd985c47fd
Jordan Lambrecht <jordan@pixelbakery.co>
2022-04-01 17:06:34 -0500

    Minor changes and code cleanup

--- components/nav/Nav_HamWrapper.tsx
+++ components/nav/Nav_HamWrapper.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
+import Nav_FullscreenMenu from './Nav_FullscreenMenu'
 import Nav_HamburgerNav from './Nav_HamburgerMenu'
 
 const Nav_HamWrapper = () => {
+  const [isHamActive, setHamToggle] = useState(false)
   return (
-    <div className=' fixed right-0 top-0 mr-6 mt-6 z-50'>
-      <Nav_HamburgerNav />
+    <div className=' fixed right-0 top-0 mr-6 mt-6 z-50 md:hidden'>
+      <Nav_HamburgerNav isActive={isHamActive} onModalUpdate={setHamToggle} />
+      <Nav_FullscreenMenu isActive={isHamActive} onModalUpdate={setHamToggle} />
     </div>
   )
 }
