7d51a81e2453890f39a212e9b15eb51d2b5511e0
Jordan Lambrecht <jordan@pixelbakery.co>
2022-04-21 12:08:35 -0500

    added blog post and project file post

--- pages/education/tutorials/[slug].tsx
+++ pages/education/tutorials/[slug].tsx
@@ -24,6 +24,7 @@ import { ArticleJsonLd, NextSeo } from 'next-seo'
 import PostHeader from '@education/Education_PostHeader'
 import Recipes_Post_Tags from '@recipes/Recipes_Post_Tags'
 import Pill from '@parts/Pill'
+import H2 from '@typography/H2'
 // Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
 // to handle import statements. Instead, you must include components in scope
@@ -127,6 +128,39 @@ export default function PostPage({ slug, allMadeToOrders, source, frontMatter })
           </div>
         </div>
       </PageSection>
+      <section className='my-4 py-16 bg-blue'>
+        <div className='grid grid-cols-1 md:grid-cols-2 px-4'>
+          <div className='col-span-1 pb-12'>
+            <div className='max-w-md mx-auto'>
+              <H2 className='mt-0 pt-0' color='cream'>
+                Support Us
+              </H2>
+              <p className='max-w-lg text-cream font-medium text-xl'>
+                If this project file helped you at all, feel free to give us a tip. All tips go
+                directly to the designer that worked on the project.
+              </p>
+            </div>
+          </div>
+          <div className=' col-span-1  flex flex-col justify-center'>
+            <div className='relative h-fit'>
+              <iframe
+                scrolling='no'
+                className='w-full  inline-block bg-transparent'
+                id='kofiframe'
+                src='https://ko-fi.com/pixelbakery/?hidefeed=true&widget=true&embed=true&preview=true&transparent-background=true'
+                style={{
+                  border: 'none',
+                  width: '100%',
+                  padding: '4px',
+                  background: 'rgba(0,0,0,0) !important',
+                }}
+                height='800'
+                title='pixelbakery'
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </Main>
   )
 }
