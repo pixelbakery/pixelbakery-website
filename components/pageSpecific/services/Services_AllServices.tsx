6aa0d6af8c9be1d0b013c2a053d785ca92cee071
Jordan Lambrecht <jordan@pixelbakery.co>
2022-04-25 11:30:51 -0500

    Removed placeholder text

--- components/pageSpecific/services/Services_AllServices.tsx
+++ components/pageSpecific/services/Services_AllServices.tsx
@@ -12,7 +12,6 @@ function Services_AllServices() {
         <div className='grid grid-cols-12 '>
           <div className='col-span-12 md:col-span-4 flex flex-col justify-center'>
             <H2 color='blue-dark'>Some of our other talents</H2>
-            <Lead className='text-pink font-extrabold text-xl'>this is a subheader</Lead>
           </div>
 
           <div className=' mx-auto flex flex-wrap col-span-12 md:grid-cols-8 -mt-2'>
