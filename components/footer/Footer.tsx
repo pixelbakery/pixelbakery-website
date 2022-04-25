def075ec2134936ba2bf7434cc5ad8b649476213
Jordan Lambrecht <jordan@pixelbakery.co>
2022-04-25 11:38:05 -0500

    Updated address

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -73,7 +73,7 @@ export const Footer: NextPage = () => {
                         target='_blank'
                         href='https://g.page/pixelbakery?share'
                       >
-                        2124 y st ste 208
+                        2124 y st ste 112
                         <br />
                         lincoln, ne 68503
                       </a>
