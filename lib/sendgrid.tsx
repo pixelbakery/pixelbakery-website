ca88faef0d7e21db08f89825a602ab50168d116d
Jordan Lambrecht <jordan@pixelbakery.co>
2022-05-04 13:02:28 -0500

    Sendgrid Debugging

--- lib/sendgrid.tsx
+++ lib/sendgrid.tsx
@@ -1,6 +1,6 @@
 import sendgrid from '@sendgrid/mail'
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY)
 
 async function sendEmail(req, res) {
   try {
