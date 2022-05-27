8263f1984aac3e136b2175899202c60afe62217d
Jordan Lambrecht <jordan@pixelbakery.co>
2022-05-27 14:25:54 -0500

    Debug

--- components/pageSpecific/caseStudies/CaseStudies_Gallery.tsx
+++ components/pageSpecific/caseStudies/CaseStudies_Gallery.tsx
@@ -28,48 +28,11 @@ function CaseStudies_Gallery() {
       </div>
     )
   }
-  //   const [lightboxOpen, setLightboxOpen] = useState(false)
-  //   const [lightboxItem, setLightboxItem] = useState(undefined)
-  //   const lightbox = useRef(null)
 
-  //   function handleLightboxItem(test: string) {
-  //     setLightboxItem(test)
-  //     console.log(test)
-  //     handleLightbox
-  //   }
-  //   function handleLightbox() {
-  //     setLightboxOpen(!lightboxOpen)
-  //   }
-
-  //   useEffect(() => {
-  //     if (!lightboxOpen) {
-  //       document.body.classList.remove('overflow-y-hidden')
-  //       gsap.to(lightbox.current, {
-  //         autoAlpha: 0,
-  //         duration: 1,
-  //       })
-  //     } else {
-  //       gsap.to(lightbox.current, {
-  //         duration: 1,
-  //         autoAlpha: 1,
-  //       })
-  //       document.body.classList.add('overflow-y-hidden')
-  //     }
-  //   }, [lightboxOpen])
-
-  //   const Lightbox = () => {
-  //     return (
-  //       <div
-  //         ref={lightbox}
-  //         onClick={handleLightbox}
-  //         className='opacity-0 bg-wine bg-opacity-60 fixed z-50 top-0 left-0 w-full h-full'
-  //       ></div>
-  //     )
-  //   }
   const Carousel = () => {
     return (
       <Swiper
-        spaceBetween={20}
+        spaceBetween={10}
         slidesPerView={1.15}
         keyboard={{
           enabled: true,
