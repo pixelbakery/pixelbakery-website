4451429782eccaead394a0bff89c7caecc6e6ba9
Jordan Lambrecht <jordan@pixelbakery.co>
2022-06-09 15:32:04 -0500

    Sharing debug

--- pages/careers/[slug].tsx
+++ pages/careers/[slug].tsx
@@ -46,7 +46,6 @@ export default function JobsPage({ slug, source, filePath, frontMatter }) {
   useEffect(() => {
     TagManager.dataLayer(tagManagerArgs)
   }, [tagManagerArgs])
-
   const datePostedISO = new Date(frontMatter.date).toISOString()
   const dateExpiredISO = addMonths(new Date(frontMatter.date), 2).toISOString()
   return (
@@ -58,7 +57,7 @@ export default function JobsPage({ slug, source, filePath, frontMatter }) {
           'Pixel Bakery is a multi-disciplinary production studio focused on animation, motion design, and commercial film production.'
         }
         openGraph={{
-          url: `https://pixelbakery.com/careers/${filePath}`,
+          // url: `https://pixelbakery.com/careers/${filePath}`,
           title: `${frontMatter.title} | Careers | Pixel Bakery`,
           description:
             'Pixel Bakery is a multi-disciplinary production studio focused on animation, motion design, and commercial film production.',
