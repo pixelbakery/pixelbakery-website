ecd9a66b1e47df3abda4c174e5afb4198a12f2e7
Jordan Lambrecht <jordan@pixelbakery.co>
2022-06-21 13:17:57 -0500

    Fixed #219

--- pages/education/index.tsx
+++ pages/education/index.tsx
@@ -18,6 +18,7 @@ function EducationPage({ allProjectFiles, allMadeToOrders }) {
   return (
     <Main className='max-w-screen overflow-x-hidden' id='educationPage'>
       <NextSeo
+        noindex={false}
         title='Education | Pixel Bakery'
         description={`Our success is a direct result of the people that we meet, whether that be our employees, our clients, or community members. As such, we believe in giving back to our community through education.`}
         openGraph={{
