01a3db2e6e31fbdbc602c95235b1957c7398618e
Jordan Lambrecht <jordan@pixelbakery.com>
2022-07-22 02:15:32 -0500

    Fixed naming convention on bios, added olli's bios, removed dead link

--- components/pageSpecific/services/ServicesOverview_More.tsx
+++ components/pageSpecific/services/ServicesOverview_More.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link'
 function ServicesOverview_More() {
   return (
     <PageSection_FullWidth id='more-services'>
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 lg:grid-cols-1 gap-4'>
         <Link href={'/services/web-hosting'} passHref>
           <div className='bg-cream flex flex-col justify-center'>
             <div
@@ -31,30 +31,6 @@ function ServicesOverview_More() {
                 <Button_Outlined
                   text={'More'}
                   link={'/services/web-hosting'}
-                  color={'blue'}
-                  chevronDirection='right'
-                />
-              </div>
-            </div>
-          </div>
-        </Link>
-        <Link href={'/services/ui-ux'} passHref>
-          <div className='bg-cream flex flex-col justify-center'>
-            <div
-              className='px-12 py-12 cursor-pointer mx-auto max-w-2xl grid grid-cols-1 lg:grid-cols-2 gap-x-24'
-              id='ui-ux'
-            >
-              <H2AndLead
-                headerColor='blue'
-                className='mb-6 2xl:mb-10 self-center'
-                headerText={'UI + UX Design'}
-                leadText={'Breathe new life into your digital storefront'}
-              ></H2AndLead>
-              <div className='self-center'>
-                <p>I think our dope website speaks for itself</p>
-                <Button_Outlined
-                  text={'More'}
-                  link={'/services/ui-ux'}
                   color={'blue'}
                   chevronDirection='right'
                 />
