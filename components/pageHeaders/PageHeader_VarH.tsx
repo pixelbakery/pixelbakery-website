f07a859b0b7cff8447c2c64d46c64e17202fbce8
Jordan Lambrecht <jordan@pixelbakery.com>
2022-08-08 18:45:46 -0500

    Moving over to Lottie-Web

--- components/pageHeaders/PageHeader_VarH.tsx
+++ components/pageHeaders/PageHeader_VarH.tsx
@@ -2,42 +2,134 @@ import { useState } from 'react'
 import dynamic from 'next/dynamic'
 import PropTypes from 'prop-types'
 
-const PageHeader_VarH_Peach = dynamic(() => import('./PageHeader_VarH_Peach'), { ssr: true })
-const PageHeader_VarH_Pink = dynamic(() => import('./PageHeader_VarH_Pink'), { ssr: true })
-const PageHeader_VarH_Pink_Light = dynamic(() => import('./PageHeader_VarH_Pink_Light'), {
-  ssr: true,
-})
-const PageHeader_VarH_Blue = dynamic(() => import('./PageHeader_VarH_Blue'), { ssr: true })
-const PageHeader_VarH_Blue_Dark = dynamic(() => import('./PageHeader_VarH_Blue_Dark'), {
-  ssr: true,
-})
-const PageHeader_VarH_Cream = dynamic(() => import('./PageHeader_VarH_Cream'), { ssr: true })
-
+function randomize(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)]
+}
 PageHeader_VarH.propTypes = {
   header: PropTypes.string,
   subheader: PropTypes.string,
   playFrames: PropTypes.array,
 }
-const patterns = [
-  PageHeader_VarH_Peach,
-  PageHeader_VarH_Pink,
-  PageHeader_VarH_Pink_Light,
-  PageHeader_VarH_Blue,
-  PageHeader_VarH_Blue_Dark,
-  PageHeader_VarH_Cream,
-]
-
-function PageHeader_VarH({ header, subheader }): JSX.Element {
-  const [randomNumber] = useState(Math.floor(Math.random() * patterns.length))
-
-  const ThisPattern = patterns[randomNumber]
+const Blue = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: false })
 
   return (
-    <>
-      <header className='bg-blue'>
-        <ThisPattern header={header} subheader={subheader} />
-      </header>
-    </>
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Blue.json')}
+      primaryColor={'blue'}
+      accentColor={'blue-dark'}
+      subheaderColor={'cream'}
+      header={header}
+      subheader={subheader}
+    />
   )
+}
+const Blue_Dark = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: true })
+  return (
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Blue_Dark.json')}
+      primaryColor={'blue-dark'}
+      accentColor={'blue'}
+      subheaderColor={'cream'}
+      subheader={subheader}
+      header={header}
+    />
+  )
+}
+const Cream = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: true })
+  return (
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Cream.json')}
+      primaryColor={'cream'}
+      accentColor={'peach'}
+      subheaderColor={'wine'}
+      subheader={subheader}
+      header={header}
+    />
+  )
+}
+const Pink = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: true })
+  return (
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Pink.json')}
+      primaryColor={'pink'}
+      accentColor={'pink-light'}
+      subheaderColor={'cream text-opacity-75'}
+      subheader={subheader}
+      header={header}
+    />
+  )
+}
+const Pink_Light = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: true })
+  return (
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Pink_Light.json')}
+      primaryColor={'pink-light'}
+      accentColor={'pink'}
+      subheaderColor={'blue-dark'}
+      subheader={subheader}
+      header={header}
+    />
+  )
+}
+const Peach = ({ header, subheader }) => {
+  const PageHeader_VarH_Test = dynamic(() => import('./PageHeader_VarH_Test'), { ssr: true })
+  return (
+    <PageHeader_VarH_Test
+      animationData={require('@data/Patterns_Peach.json')}
+      primaryColor={'peach'}
+      accentColor={'egg'}
+      subheaderColor={'egg'}
+      subheader={subheader}
+      header={header}
+    />
+  )
+}
+
+function PageHeader_VarH({ header, subheader }) {
+  const [arr] = useState([
+    <Blue header={header} subheader={subheader} />,
+    <Blue_Dark header={header} subheader={subheader} />,
+    <Pink header={header} subheader={subheader} />,
+    <Pink_Light header={header} subheader={subheader} />,
+
+    <Cream header={header} subheader={subheader} />,
+    <Peach header={header} subheader={subheader} />,
+  ])
+  const [Rand] = useState(randomize(arr))
+  // PageHeader_VarH_Pink,
+  // PageHeader_VarH_Pink_Light,
+  // PageHeader_VarH_Blue,
+  // PageHeader_VarH_Blue_Dark,
+  // PageHeader_VarH_Cream,
+
+  // useEffect(() => {
+  //   if (element.current) {
+  //     setRandomNumber(Math.floor(Math.random() * patterns.length))
+  //     console.log(randomNumber)
+
+  //     element.current = (
+  //       <PageHeader_VarH_Test
+  //         animationData={patterns[0].pattern}
+  //         header={header}
+  //         subheader={subheader}
+  //         primaryColor={patterns[0].primaryColor}
+  //         accentColor={patterns[0].accentColor}
+  //         subheaderColor={patterns[0].subheaderColor}
+  //       />
+  //     )
+  //   }
+  //   return () => {
+  //     element.current = null
+  //   }
+  // }, [patterns])
+
+  // const ThisPattern = patterns[randomNumber]
+
+  return <header className='bg-blue'>{Rand}</header>
 }
 export default PageHeader_VarH
---
+++ components/pageHeaders/PageHeader_VarH_Test.tsx
@@ -0,0 +1,88 @@
+// import Lottie from 'react-lottie-player'
+import classNames from 'classnames'
+import PageHeader_TextBox from './PageHeader_TextBox'
+import { useEffect, useRef } from 'react'
+import Lead from '@typography/Lead'
+import lottie from 'lottie-web'
+
+interface LottieProps {
+  animationData: any
+  width: number
+  height: number
+}
+
+const Lottie = ({ animationData }: LottieProps) => {
+  const element = useRef<HTMLDivElement>(null)
+  const lottieInstance = useRef<any>()
+
+  useEffect(() => {
+    if (element.current) {
+      lottieInstance.current?.destroy()
+      lottieInstance.current = lottie.loadAnimation({
+        container: element.current,
+        renderer: 'svg',
+        loop: true,
+        // autoplay: true,
+
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice',
+        },
+        animationData: animationData,
+      })
+    }
+
+    lottieInstance.current.playSegments(
+      [
+        [0, 23],
+        [24, 95],
+      ],
+      true,
+    )
+
+    return () => {
+      lottieInstance.current?.destroy()
+      lottieInstance.current = null
+    }
+  }, [animationData])
+
+  if (!animationData) {
+    return (
+      <div className='w-full h-full flex flex-col justify-center'>
+        <Lead color='cream' className='self-center text-center'>
+          Loading
+        </Lead>
+      </div>
+    )
+  }
+  return <div style={{ height: '100%', width: '100%' }} ref={element} />
+}
+
+const PageHeader_VariableHeight = ({
+  animationData,
+  header,
+  primaryColor,
+  accentColor,
+  subheaderColor,
+  subheader,
+}) => {
+  return (
+    <div
+      className={classNames('relative overflow-hidden lander-variableHeight my-4', [
+        `bg-${accentColor}`,
+      ])}
+      id='topOfPage'
+    >
+      <div className=' z-0 absolute  top-0 left-0 w-full h-full overflow-hidden lottie' id='lottie'>
+        <Lottie animationData={animationData} width={100} height={100} />
+      </div>
+      <PageHeader_TextBox
+        header={header}
+        primaryColor={primaryColor}
+        accentColor={accentColor}
+        subheader={subheader}
+        subheaderColor={subheaderColor}
+      />
+    </div>
+  )
+}
+export default PageHeader_VariableHeight
