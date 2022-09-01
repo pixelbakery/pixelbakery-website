a6f08784adbe5dd9f1597ea383b18216303908a3
Jordan Lambrecht <jordan@pixelbakery.com>
2022-09-01 00:53:27 -0500

    Added a 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,27 @@
+import Main from '@parts/Main'
+import { NextSeo } from 'next-seo'
+import Image from 'next/image'
+function FourOhFour() {
+  return (
+    <Main>
+      <NextSeo noindex={true} title='404 Page Not Found' />
+      <section className='lander' id='topOfPage'>
+        <div className='mx-auto flex flex-col justify-center max-w-4xl h-full'>
+          <h1 className='mb-12 text-center text-4xl text-blue'>500 - Server-side error occurred</h1>
+          <Image
+            src={`/img/imindanger.webp`}
+            layout='responsive'
+            width='480px'
+            height='269px'
+            placeholder='blur'
+            blurDataURL={`/img/imindanger.webp`}
+            className='w-full'
+            alt={"I'm in danger"}
+          />
+        </div>
+      </section>
+    </Main>
+  )
+}
+
+export default FourOhFour
