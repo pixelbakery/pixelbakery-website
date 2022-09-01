deef80b6300d41f0f3a3fc48aa9242d7461a4786
Jordan Lambrecht <jordan@pixelbakery.com>
2022-09-01 00:51:50 -0500

    ADDED: fallback loading component

---
+++ components/utility/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className='-z-10 absolute left-0 top-0 w-full h-full bg-blue text-cream flex flex-col justify-center text-center text-lg'>
+      🙃 Loading...
+    </div>
+  )
+}
+
+export default Loading
