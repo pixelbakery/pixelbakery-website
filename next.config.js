cc3d65b6b21b35923ccf7d4eb93a556d8cc84fed
Jordan Lambrecht <jordan@pixelbakery.com>
2022-10-05 18:07:06 -0500

    More sec headers

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ module.exports = () => {
             },
             {
               key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
+              value: 'strict-origin-when-cross-origin',
             },
             {
               key: 'Permissions-Policy',
