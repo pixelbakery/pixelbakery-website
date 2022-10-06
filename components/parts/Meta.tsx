0af167661282d8ad892382260b8ade7dd83a1747
Jordan Lambrecht <jordan@pixelbakery.com>
2022-10-06 11:26:40 -0500

    Whoops

--- components/parts/Meta.tsx
+++ components/parts/Meta.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head'
 const Meta = () => {
   return (
     <Head>
-      <Link hrefLang={'en-US'} rel='apple-touch-icon' sizes='32x32' href='/favicon.png' />
-      <Link hrefLang={'en-US'} rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
-      <Link hrefLang={'en-US'} rel='icon' type='image/ico' sizes='32x32' href='/favicon.ico' />
-      <Link hrefLang={'en-US'} rel='shortcut icon' href='/favicon.ico' />
+      <link rel='apple-touch-icon' sizes='32x32' href='/favicon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
+      <link rel='icon' type='image/ico' sizes='32x32' href='/favicon.ico' />
+      <link rel='shortcut icon' href='/favicon.ico' />
     </Head>
   )
 }
