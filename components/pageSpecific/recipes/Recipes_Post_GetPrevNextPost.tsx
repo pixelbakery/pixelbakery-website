0caae8d1b87a652b5d0a4fd3eeec9ce654e98d5f
Jordan Lambrecht <jordan@pixelbakery.com>
2022-10-13 03:08:52 -0500

    Styling

--- components/pageSpecific/recipes/Recipes_Post_GetPrevNextPost.tsx
+++ components/pageSpecific/recipes/Recipes_Post_GetPrevNextPost.tsx
@@ -1,6 +1,7 @@
 import { ChevronRightIcon } from '@images/UI_Icons'
 import InnerWrapper from '@parts/InnerWrapper'
 import PageSection from '@parts/PageSection'
+import Lead from '@typography/Lead'
 import next from 'next'
 import Link from 'next/link'
 
@@ -8,42 +9,62 @@ function Recipes_Post_GetPrevNextPost({ prev, next }) {
   return (
     <PageSection color={'pink-light'} id={'next-post'}>
       <InnerWrapper className='py-2 my-2'>
-        <div className='flex justify-between'>
-          <Link
-            hrefLang={'en-US'}
-            as={`/recipes/${prev.filePath.replace(/\.mdx?$/, '')}`}
-            href={`/recipes/[slug]`}
-          >
-            <a hrefLang={'en-US'} className='flex'>
-              <div className='w-20 self-center text-peach rotate-180'>
-                <ChevronRightIcon />
-              </div>
-              <div className='flex flex-col justify-center'>
-                <p className='text-peach font-semibold text-xl leading-none my-0 py-0 max-w-md'>
-                  {prev.data.title}
-                </p>
-                <span className='mb-1 italic text-wine'>By {prev.data.author.name}</span>
-              </div>
-            </a>
-          </Link>
-          <Link
-            hrefLang={'en-US'}
-            as={`/recipes/${next.filePath.replace(/\.mdx?$/, '')}`}
-            href={`/recipes/[slug]`}
-          >
-            <a hrefLang={'en-US'} className='flex'>
-              <div className='flex flex-col justify-center'>
-                <p className='text-peach font-semibold text-right text-xl leading-none my-0 py-0 max-w-md'>
-                  {next.data.title}
-                </p>
-                <span className='mb-1 italic text-wine'>By {next.data.author.name}</span>
-              </div>
+        <div className='flex justify-between gap-x-12'>
+          <div className='w-1/2'>
+            <Link
+              hrefLang={'en-US'}
+              as={`/recipes/${prev.filePath.replace(/\.mdx?$/, '')}`}
+              href={`/recipes/[slug]`}
+            >
+              <a hrefLang={'en-US'} className='flex'>
+                <div className='w-12 self-center text-peach rotate-180 h-full flex flex-col justify-center'>
+                  <i className='text-3xl p-2'>
+                    <ChevronRightIcon />
+                  </i>
+                </div>
 
-              <div className='w-20 self-center text-peach'>
-                <ChevronRightIcon />
-              </div>
-            </a>
-          </Link>
+                <div className='self-center'>
+                  <Lead
+                    className='text-peach font-semibold text-lg md:text-md leading-none  my-0 py-0 h-full '
+                    noMargins={true}
+                  >
+                    {prev.data.title}
+                  </Lead>
+                  <p className='text-wine-400 font-medium text-xs md:text-sm italic leading-none mb-0 py-0 mt-1'>
+                    {prev.data.author.name}
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className='w-1/2'>
+            <Link
+              hrefLang={'en-US'}
+              as={`/recipes/${next.filePath.replace(/\.mdx?$/, '')}`}
+              href={`/recipes/[slug]`}
+            >
+              <a hrefLang={'en-US'} className='flex'>
+                <div className='self-center w-fit'>
+                  <Lead
+                    className='text-peach font-semibold text-lg md:text-md leading-none  my-0 py-0 h-full '
+                    noMargins={true}
+                  >
+                    {next.data.title}
+                  </Lead>
+                  <p className='text-wine-400 font-medium text-xs md:text-sm italic leading-none mb-0 py-0 mt-1'>
+                    {next.data.author.name}
+                  </p>
+                </div>
+
+                <div className=' text-peach self-center h-full flex flex-col justify-center'>
+                  {' '}
+                  <i className='text-3xl p-2'>
+                    <ChevronRightIcon />
+                  </i>
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
       </InnerWrapper>
     </PageSection>
