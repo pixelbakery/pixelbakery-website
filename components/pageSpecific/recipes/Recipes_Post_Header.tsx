b2b8cb78c021d9da5b9106e1a5072355009dd8ea
Jordan Lambrecht <jordan@pixelbakery.com>
2022-11-10 19:46:05 -0600

    ADDED: team avatars

--- components/pageSpecific/recipes/Recipes_Post_Header.tsx
+++ components/pageSpecific/recipes/Recipes_Post_Header.tsx
@@ -47,13 +47,13 @@ const Recipes_Post_Header = ({ date, frontMatter, matchingBio, readTime }: Props
               >
                 <Image
                   placeholder='blur'
-                  blurDataURL={`${process.env.NEXT_PUBLIC_IMG_PREFIX}${matchingBio.data.headshotSmiling}`}
-                  layout='fill'
-                  objectFit='cover'
+                  blurDataURL={`${process.env.NEXT_PUBLIC_IMG_PREFIX}${matchingBio.data.avatar}`}
+                  width={124}
+                  height={124}
                   alt={frontMatter.author.name}
-                  quality={20}
-                  className='object-top scale-175 sc'
-                  src={`${process.env.NEXT_PUBLIC_IMG_PREFIX}${matchingBio.data.headshotSmiling}`}
+                  quality={75}
+                  className='w-full h-full object-cover'
+                  src={`${process.env.NEXT_PUBLIC_IMG_PREFIX}${matchingBio.data.avatar}`}
                 />
               </Link>
             ) : (
@@ -62,11 +62,10 @@ const Recipes_Post_Header = ({ date, frontMatter, matchingBio, readTime }: Props
                 <Image
                   placeholder='blur'
                   blurDataURL={`${process.env.NEXT_PUBLIC_IMG_PREFIX}/img/placeholder01.png`}
-                  layout='fill'
-                  objectFit='cover'
+                  fill={true}
                   alt={frontMatter.author.name}
                   quality={25}
-                  className='object-top scale-175 sc'
+                  className='object-cover object-top scale-175'
                   src={`${process.env.NEXT_PUBLIC_IMG_PREFIX}/img/placeholder01.png`}
                 />
               </div>
