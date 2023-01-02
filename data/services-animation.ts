1a2634d434dab5a08bb248fa203dfa5974730898
Jordan Lambrecht <jordan@pixelbakery.com>
2023-01-02 17:57:52 -0600

    Added content to animation services page

--- data/services-animation.ts
+++ data/services-animation.ts
@@ -3,16 +3,16 @@
 export default [
   {
     service: 'Kinetic Typography',
-    desc: 'Kinetic typography is  an animation technique used to make text move in numerous ways – whether that be expanding, shrinking, or moving in slow-motion, to name a few. The effect adds emphasis to the text’s meaning, or can simply add an element of motion to your videos.',
+    desc: 'Kinetic typography is an animation technique used to make text move in numerous ways – whether that be expanding, shrinking, or moving in slow-motion, to name a few. The effect adds emphasis to the text’s meaning, or can simply add an element of motion to your videos.',
     icon: '🥓',
   },
   {
-    service: 'Product Sizzles',
+    service: 'Product Demonstrations',
     desc: 'An animated sizzle reel is a short (three minutes or less) promotional video that highlights a specific product or service. This is edited with short animation, graphics, and text clips. Tied together with a groovy soundtrack leaves your audience wanting to see more of your brand.',
     icon: '🍳',
   },
   {
-    service: 'Explainers',
+    service: 'Explainer Videos',
     desc: 'Explainer videos break down a complicated concept into something easily digestible for your audience. Using animation and voiceover, we can help simplify your business ideas, products, and the story you want to tell.',
     icon: '🧇',
   },
@@ -22,7 +22,7 @@ export default [
     icon: '🍄',
   },
   {
-    service: 'Living Infographics',
+    service: 'Data Visualization',
     desc: 'Infographics bring your data and information to life visually. When these are done correctly, they are able to transfer data into stories, making the complex information easy for your audience to digest. By adding in great design, infographics are visually engaging and should make the user curious to learn and see more.',
     icon: '🫐',
   },
@@ -35,5 +35,20 @@ export default [
     service: 'Independent Film',
     desc: "Let's finally get that YouTube channel you've always dreamed of making off the ground.",
     icon: '🥪',
+  },
+  {
+    service: 'Interactive Content',
+    desc: 'Animation can also be used to create interactive content, such as games or quizzes. This can be a great way to engage and entertain audiences while also gathering valuable data and insights.',
+    icon: '🍽️',
+  },
+  {
+    service: 'Social Media Ads',
+    desc: 'Animation has become a popular choice for creating short, attention-grabbing ads for social media marketing campaigns. These ads can be used to promote products, drive traffic to a website, or increase social media followers.',
+    icon: '🧃',
+  },
+  {
+    service: 'Branded Entertainment',
+    desc: 'We use Motion Graphics to create branded entertainment, such as web series or animated shorts. These types of content can help build brand awareness and loyalty by creating a more personal and emotional connection with the audience.',
+    icon: '🍿',
   },
 ]
