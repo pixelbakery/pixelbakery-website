53be3ee6c01d7633598daa08697c6d4aa9d5f603
Jordan Lambrecht <jordan@pixelbakery.com>
2023-08-31 17:04:53 -0500

    Changed sitemap policies

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -13,4 +13,64 @@ module.exports = {
     '/zoom',
     '/book',
   ],
+  additionalPaths: async () => {
+    const result = []
+
+    // all possible values
+    result.push(
+      {
+        loc: '/store/product/wavy-hoodie',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/tiny-sticker-pack',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/lincoln-flag-pin',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/long-sleeve-tshirt',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/black-mug',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/mug-white',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/short-sleeve-pride-tee',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/store/product/queer-tote',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+    )
+
+    // using transformation from the current configuration
+    // result.push(await config.transform(config, '/additional-page-3'))
+
+    return result
+  },
 }
