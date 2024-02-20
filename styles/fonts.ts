4f2733fdd0b3f67ebe4a7adfb89028fc325bfae4
Jordan Lambrecht <jordan@pixelbakery.com>
2024-02-19 21:51:07 -0600

    test: debugging font paths in production

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -5,8 +5,9 @@ import localFont from 'next/font/local'
 /*****************************************/
 const dempsey = localFont({
   display: 'swap',
+  preload: true,
   variable: '--font-dempsey',
-  src: '../public/fonts/dempsey/dempsey-regular.woff2',
+  src: './fonts/dempsey/dempsey-regular.woff2',
   weight: '400', // font-semibold
   style: 'normal',
 })
@@ -15,10 +16,12 @@ const dempsey = localFont({
 /*               POPPINS                 */
 /*****************************************/
 const poppins = localFont({
+  preload: true,
+  display: 'swap',
   src: [
     // 400, font-normal
     {
-      path: '../public/fonts/poppins/Poppins-Regular.woff2',
+      path: './fonts/poppins/Poppins-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
@@ -29,56 +32,56 @@ const poppins = localFont({
     },
     // // 500, font-medium
     {
-      path: '../public/fonts/poppins/Poppins-Medium.woff2',
+      path: './fonts/poppins/Poppins-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/poppins/Poppins-MediumItalic.woff2',
+      path: './fonts/poppins/Poppins-MediumItalic.woff2',
       weight: '500',
       style: 'italic',
     },
     // //  600, font-semibold
     {
-      path: '../public/fonts/poppins/Poppins-SemiBold.woff2',
+      path: './fonts/poppins/Poppins-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/poppins/Poppins-SemiBoldItalic.woff2',
+      path: './fonts/poppins/Poppins-SemiBoldItalic.woff2',
       weight: '600',
       style: 'italic',
     },
     // // 700, font-bold
     {
-      path: '../public/fonts/poppins/Poppins-Bold.woff2',
+      path: './fonts/poppins/Poppins-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/poppins/Poppins-BoldItalic.woff2',
+      path: './fonts/poppins/Poppins-BoldItalic.woff2',
       weight: '700',
       style: 'italic',
     },
     // // 800, font-extrabold
     {
-      path: '../public/fonts/poppins/Poppins-ExtraBold.woff2',
+      path: './fonts/poppins/Poppins-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/fonts/poppins/Poppins-ExtraBoldItalic.woff2',
+      path: './fonts/poppins/Poppins-ExtraBoldItalic.woff2',
       weight: '800',
       style: 'italic',
     },
     // // 900, font-black
     {
-      path: '../public/fonts/poppins/Poppins-Black.woff2',
+      path: './fonts/poppins/Poppins-Black.woff2',
       weight: '900',
       style: 'normal',
     },
     {
-      path: '../public/fonts/poppins/Poppins-BlackItalic.woff2',
+      path: './fonts/poppins/Poppins-BlackItalic.woff2',
       weight: '900',
       style: 'italic',
     },
@@ -91,41 +94,44 @@ const poppins = localFont({
 /******************************************/
 
 const gilroy = localFont({
+  preload: true,
+  display: 'swap',
   src: [
     // SemiBold (600)
     {
-      path: '../public/fonts/gilroy/gilroy-bold.woff2',
-      weight: '600', // font-semibold
+      path: './fonts/gilroy/gilroy-bold.woff2',
+      weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/gilroy/gilroy-bolditalic.woff2',
-      weight: '600', // font-semibold
+      path: './fonts/gilroy/gilroy-bolditalic.woff2',
+      weight: '600',
       style: 'italic',
     },
-    // // Bold (700)
+    // Bold (700)
     {
-      path: '../public/fonts/gilroy/gilroy-extrabold.woff2',
-      weight: '700', // font-bold
+      path: './fonts/gilroy/gilroy-extrabold.woff2',
+      weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/gilroy/gilroy-extrabolditalic.woff2',
-      weight: '700', // font-bold
+      path: './fonts/gilroy/gilroy-extrabolditalic.woff2',
+      weight: '700',
       style: 'italic',
     },
-    // // ExtraBold (800)
+    // ExtraBold (800)
     {
-      path: '../public/fonts/gilroy/gilroy-black.woff2',
-      weight: '800', // font-extrabold
+      path: './fonts/gilroy/gilroy-black.woff2',
+      weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/fonts/gilroy/gilroy-blackitalic.woff2',
-      weight: '800', // font-extrabold
+      path: './fonts/gilroy/gilroy-blackitalic.woff2',
+      weight: '800',
       style: 'italic',
     },
   ],
+
   variable: '--font-gilroy',
 })
 
