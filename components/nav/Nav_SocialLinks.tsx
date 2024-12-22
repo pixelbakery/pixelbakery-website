a44fff2493dc522ae2b6072166a76e10ccd99fa0
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-22 04:39:20 -0600

    chore: removed discord and twitch social links from main navigation

--- components/nav/Nav_SocialLinks.tsx
+++ components/nav/Nav_SocialLinks.tsx
@@ -8,7 +8,7 @@ interface SocialLinksProps {
 }
 function Nav_SocialLinks({ color, textSize }: SocialLinksProps) {
   return (
-    <div className='max-w-xs mx-auto flex justify-between gap-3 px-8'>
+    <div className='flex justify-between max-w-xs gap-3 px-8 mx-auto'>
       <SocialIcon
         iconName={'Instagram'}
         color={`${color}`}
@@ -21,24 +21,14 @@ function Nav_SocialLinks({ color, textSize }: SocialLinksProps) {
         size={`${textSize}`}
         href={'https://www.youtube.com/c/pixelbakery'}
       />
-      <SocialIcon
-        iconName={'Discord'}
-        color={`${color}`}
-        size={`${textSize}`}
-        href={'https://discord.gg/DBxgDGJzAg'}
-      />
+
       <SocialIcon
         iconName={'Vimeo'}
         color={`${color}`}
         size={`${textSize}`}
         href={'https://vimeo.com/pixelbakery'}
       />
-      <SocialIcon
-        iconName={'Twitch'}
-        color={`${color}`}
-        size={`${textSize}`}
-        href={'https://twitch.tv/pixelbakery'}
-      />
+
       <SocialIcon
         iconName={`LinkedIn`}
         color={`${color}`}
