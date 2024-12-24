d368a9efb321789edadddabc08ab9645f4672f51
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-23 20:56:30 -0600

    refactor: rewrote the footer

--- components/footer/Footer_Objects_SocialLinks.tsx
+++ components/footer/Footer_Objects_SocialLinks.tsx
@@ -2,7 +2,7 @@ import Nav_SocialLinks from '@nav/Nav_SocialLinks'
 
 const Footer_Objects_SocialLinks = () => {
   return (
-    <div className='w-full lg:py-4 xl:my-3'>
+    <div className='w-full lg:py-4'>
       <div className='w-full' id='footer-social'>
         <Nav_SocialLinks color={'cream'} textSize='2xl' />
       </div>
