0b9fc1d126e1988d60833418e7856e8a5ee23169
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-28 23:04:35 -0600

    style: font fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,15 +7,17 @@ function Page_FourOhFour() {
     <>
       <NextSeo noindex={true} title='404 Page Not Found' />
       <section className='lander' id='topOfPage'>
-        <div className='mx-auto flex flex-col justify-center max-w-4xl h-full'>
-          <h1 className='mb-12 text-center text-4xl text-blue'>404: Page Not Found</h1>
+        <div className='flex flex-col justify-center h-full max-w-4xl mx-auto'>
+          <h1 className='mb-12 text-4xl font-extrabold text-center font-geologica text-blue'>
+            404: Page Not Found
+          </h1>
           <Image
             src={`${process.env.NEXT_PUBLIC_IMG_PREFIX}/img/imindanger.webp`}
             width={480}
             height={269}
             placeholder='blur'
             blurDataURL={`${process.env.NEXT_PUBLIC_IMG_PREFIX}/img/imindanger.webp`}
-            className='w-full object-contain'
+            className='object-contain w-full'
             alt={"I'm in danger"}
           />
         </div>
