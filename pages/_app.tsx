413531c7776aad6c12b968d78c8fe03c3a1baada
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-31 02:02:06 -0600

    feat: added Plausible for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import '@styles/fonts'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import PlausibleProvider from 'next-plausible'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -15,13 +16,23 @@ type AppPropsWithLayout = AppProps & {
 }
 
 //future self: leave this as a function and not a const
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <>
-      {getLayout(<Component {...pageProps} />)}
-      <SpeedInsights />
-      <GoogleAnalytics gaId={(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string) || ''} />
+      <PlausibleProvider
+        domain='pixelbakery.com'
+        trackOutboundLinks
+        trackLocalhost={process.env.NODE_ENV !== 'production'}
+        selfHosted
+        customDomain='https://analytics.jordy.world'
+        // enabled={process.env.NODE_ENV === "production"}
+        enabled
+      >
+        {getLayout(<Component {...pageProps} />)}
+        <SpeedInsights />
+        <GoogleAnalytics gaId={(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string) || ''} />
+      </PlausibleProvider>
     </>
   )
 }
