31741b076e29419858f0758c159209b373873582
Jordan Lambrecht <jordan@pixelbakery.com>
2024-12-30 23:55:57 -0600

    refactor: removed dynamic imports, code cleanup, type defs

--- pages/about/[slug].tsx
+++ pages/about/[slug].tsx
@@ -11,6 +11,7 @@ import {
   CASESTUDIES_PATH,
 } from '@lib/mdxUtils'
 import { serialize } from 'next-mdx-remote/serialize'
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 //Utilities & Components imports
 import remarkGfm from 'remark-gfm'
@@ -21,21 +22,40 @@ import {
   About_Team_SEO,
   About_Team_Details,
 } from '@about/index'
-import dynamic from 'next/dynamic'
-import { GetStaticPaths } from 'next'
+
+import { GetStaticPaths, GetStaticProps, GetStaticPropsResult } from 'next'
 import type { ReactElement } from 'react'
 import Layout_Defaualt from 'components/layouts/Layout_Default'
+import About_Team_MatchingPosts from '@about/About_Team_MatchingPosts'
+import About_Team_MatchingCaseStudies from '@about/About_Team_MatchingCaseStudies'
+// -- Types ---------------------------------------------------------------------
 
-const About_Team_MatchingPosts = dynamic(() => import('@about/About_Team_MatchingPosts'), {
-  ssr: false,
-})
+interface FrontMatterProps {
+  active?: boolean
+  name?: string
+  [key: string]: any
+}
 
-const About_Team_MatchingCaseStudies = dynamic(
-  () => import('@about/About_Team_MatchingCaseStudies'),
-  {
-    ssr: false,
-  },
-)
+interface ParamsProps {
+  params: {
+    slug: string
+  }
+}
+interface FileData {
+  filePath: string
+  data: FrontMatterProps & { [key: string]: any }
+}
+
+interface PagePeopleProps {
+  matchingCaseStudies: FileData[]
+  slug: string
+  source: MDXRemoteSerializeResult
+  frontMatter: FrontMatterProps
+  matchingAuthorPosts: FileData[]
+  prevIndex: FileData | null
+  nextIndex: FileData | null
+}
+
 function Page_People({
   matchingCaseStudies,
   slug,
@@ -44,9 +64,7 @@ function Page_People({
   matchingAuthorPosts,
   prevIndex,
   nextIndex,
-}) {
-  // console.log(new_data[0].length)
-
+}: PagePeopleProps) {
   return (
     <>
       <About_Team_SEO frontMatter={frontMatter} slug={slug} />
@@ -58,20 +76,27 @@ function Page_People({
         name={frontMatter.name}
       />
       <About_Team_PrevNext
-        active={frontMatter.active}
+        active={frontMatter.active as boolean}
         prev={prevIndex}
         next={nextIndex}
-        name={frontMatter.name}
+        name={frontMatter.name || ''}
       />
     </>
   )
 }
+// -- getStaticProps -----------------------------------------------------------
 
-export async function getStaticProps({ params }) {
-  // Main content
+export const getStaticProps: GetStaticProps<PagePeopleProps, { slug: string }> = async ({
+  params,
+}) => {
+  if (!params?.slug) {
+    return { notFound: true }
+  }
+
   const temp = path.join(PEOPLE_PATH, `${params.slug}.mdx`)
-  const source = fs.readFileSync(temp)
-  const { content, data } = matter(source)
+  const fileContent = fs.readFileSync(temp)
+  const { content, data } = matter(fileContent)
+
   const mdxSource = await serialize(content, {
     mdxOptions: {
       remarkPlugins: [remarkGfm],
@@ -81,7 +106,6 @@ export async function getStaticProps({ params }) {
     scope: data,
   })
 
-  // Find matching posts written by this person
   const matchingAuthorPosts = postFilePaths
     .map((filePath) => {
       const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
@@ -90,78 +114,65 @@ export async function getStaticProps({ params }) {
       return { data, filePath }
     })
     .sort((post1, post2) => (post1.data.date > post2.data.date ? -1 : 1))
-    .filter((p) => p.data.author.name.toUpperCase() === data.name.toUpperCase())
+    .filter((p) => p.data.author?.name?.toUpperCase() === data.name.toUpperCase())
     .slice(0, 11)
 
   const allPeople = peopleFilePaths
     .map((filePath) => {
       const source = fs.readFileSync(path.join(PEOPLE_PATH, filePath))
       const { data } = matter(source)
-
       return { filePath, data }
     })
     .filter((cs) => cs.data.active === true)
     .sort((cs1, cs2) => (cs1.data.name < cs2.data.name ? -1 : 1))
 
-  //Find the previous and next person on the roster, alphabetically by last name.
-  let thisIndex,
-    prevIndex,
-    nextIndex = null
+  let thisIndex: number | undefined
+  let prevIndex: FileData | null = null
+  let nextIndex: FileData | null = null
 
-  if (data.active != false) {
-    allPeople.map((p, index) => {
+  if (data.active !== false) {
+    allPeople.forEach((p, index) => {
       if (p.data.name === data.name) {
         thisIndex = index
       }
     })
 
-    if (thisIndex != undefined && thisIndex === 0)
+    if (thisIndex !== undefined && thisIndex === 0) {
       prevIndex = allPeople[Object.keys(allPeople).length - 1]
-    else prevIndex = allPeople[thisIndex - 1]
+    } else if (thisIndex !== undefined) {
+      prevIndex = allPeople[thisIndex - 1]
+    }
 
-    if (thisIndex != undefined && thisIndex === Object.keys(allPeople).length - 1)
+    if (thisIndex !== undefined && thisIndex === Object.keys(allPeople).length - 1) {
       nextIndex = allPeople[0]
-    else nextIndex = allPeople[thisIndex + 1]
-  } else (thisIndex = null), (nextIndex = null), (prevIndex = null)
-
-  //End of prev/next search
-
-  // Find matching case studies this person has credits in
+    } else if (thisIndex !== undefined) {
+      nextIndex = allPeople[thisIndex + 1]
+    }
+  }
 
   const matchingCaseStudies = caseStudyFilePaths
     .map((filePath) => {
       const source = fs.readFileSync(path.join(CASESTUDIES_PATH, filePath))
       const { data } = matter(source)
       data.date = JSON.parse(JSON.stringify(data.date))
-
       return { data, filePath }
     })
     .sort((post1, post2) => (post1.data.date > post2.data.date ? -1 : 1))
     .filter((cs) => cs.data.active === true)
     .filter((c) => {
       let bool = false
-      c.data.credits.forEach((cs) => {
-        if (cs.name.toLowerCase() === data.name.toLowerCase()) {
-          bool = true
-        }
+      c.data.credits?.forEach((credit: { name: string }) => {
+        if (credit.name.toLowerCase() === data.name.toLowerCase()) bool = true
       })
       return bool
     })
-  // .filter((p) => p.data.credits.includes('Jordan Lambrecht'))
-
-  // matchingCaseStudies.forEach((cs) => {
-  //   let test = cs.data.credits
-  //   if (matches(test) === true) {
-  //     console.log(cs.data.title)
-  //   }
-  // })
 
   return {
     props: {
-      matchingCaseStudies: matchingCaseStudies,
-      matchingAuthorPosts: matchingAuthorPosts,
-      nextIndex: nextIndex,
-      prevIndex: prevIndex,
+      matchingCaseStudies,
+      matchingAuthorPosts,
+      nextIndex,
+      prevIndex,
       slug: params.slug,
       source: mdxSource,
       frontMatter: data,
@@ -171,7 +182,7 @@ export async function getStaticProps({ params }) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = peopleFilePaths
-    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((filePath) => filePath.replace(/\.mdx?$/, ''))
     .map((slug) => ({ params: { slug } }))
 
   return {
