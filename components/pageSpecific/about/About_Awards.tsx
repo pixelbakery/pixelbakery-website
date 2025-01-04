6c9714a28a43737b1b82f04e82d8cf312206c250
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-04 17:23:30 -0600

    feat: added portfolio links to relevant awards

--- components/pageSpecific/about/About_Awards.tsx
+++ components/pageSpecific/about/About_Awards.tsx
@@ -1,35 +1,61 @@
-import InnerWrapper from '@parts/InnerWrapper'
-import PageSection from '@parts/PageSection'
+import { InnerWrapper, PageSection } from '@parts'
 import awards from '@data/awards'
-import H2 from '@typography/H2'
+import { H2AndLead } from '@typography'
+import Link from 'next/link'
 
 const About_Awards = () => {
   return (
     <PageSection color={'blue'} id='awards'>
       <InnerWrapper>
-        <div className=' grid grid-cols-1 xl:grid-cols-3 gap-2 md:gap-12 xl:gap-32'>
+        <div className='grid grid-cols-1 gap-2 xl:grid-cols-3 md:gap-12 xl:gap-32'>
           <div className='col-span-1'>
-            <H2 color='blue-dark'>
-              awards and recognition<span className='hidden lg:inline'>*</span>
-            </H2>
-            <div className='-mt-8 text-2xl text-cream font-extrabold'>swoosh.</div>
-            <p className='mb-0 pb-0 max-w-lg mt-6 text-xs text-cream font-medium'>
-              *awards are stupid and meaningless, but idk, i guess everything is. might as well
-              smoke &apos;em if you got &apos;em, right?
+            <H2AndLead
+              headerColor='blue-dark'
+              leadColor='cream'
+              headerText='Awards and Recognition*'
+              leadText='Swoosh.'
+            />
+
+            <p className='max-w-lg pb-0 mt-6 mb-0 text-xs font-medium text-cream'>
+              *Awards are stupid and meaningless, but idk, I guess everything is though. Might as
+              well smoke &apos;em if you got &apos;em, right?
             </p>
           </div>
-          <div className='col-span-2  flex flex-col justify-between mx-auto w-full '>
+          <div className='flex flex-col justify-between w-full col-span-1 mx-auto lg:col-span-2 '>
             <ul
-              className='w-fit self-end text-cream font-poppins text-sm sm:text-sm md:text-lg font-medium md:font-bold  relative awardsList'
+              className='relative text-sm font-medium lg:self-end w-fit text-cream font-poppins sm:text-sm md:text-lg md:font-medium awardsList'
               id='awardsList1'
             >
               {awards.map((award, index) => {
                 return (
-                  <li key={index + award.project} className='my-4 '>
-                    {award.award != '' ? `${award.award} ` : ''}
-                    {award.category != '' ? `/ ${award.category} ` : ''}
-                    {award.project != '' ? `/ ${award.project} ` : ''}
-                    {award.client != '' ? `/ ${award.client} ` : ''}
+                  <li
+                    key={index + award.project}
+                    className='pb-2 my-4 border-b-2 border-blue-dark text-blue-dark'
+                  >
+                    {award.award != '' ? (
+                      <span className='text-2xl font-geologica'>{award.award}</span>
+                    ) : (
+                      ''
+                    )}
+                    <p className='pt-0 my-0 ml-8 leading-none text-blue-dark'>
+                      {award.category != '' ? `${award.category}, ` : ''}
+                      {award.project != '' ? (
+                        award.projectLink ? (
+                          <>
+                            <Link
+                              href={award.projectLink}
+                              className='underline hover:text-blue-light text-blue-dark'
+                            >
+                              {award.project}
+                            </Link>
+                            ,
+                          </>
+                        ) : (
+                          `${' '}${award.project},`
+                        )
+                      ) : null}
+                      {award.client != '' && `${' '}${award.client}`}
+                    </p>
                   </li>
                 )
               })}
