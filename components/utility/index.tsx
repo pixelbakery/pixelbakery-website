7e97df761a2650dfe383ad9620761582db3f292e
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-05 00:40:53 -0600

    chore: minor code cleanup

--- components/utility/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as BackToTop } from './BackToTop'
-export * from './ContactForm_Parts'
-export { default as Loading } from './Loading'
--- lib/colSpanHelper.ts
+++
@@ -1,6 +0,0 @@
-export const WidthHelper = {
-  1: 'col-span-1',
-  '': '',
-}
-
-export type ChevronType = keyof typeof WidthHelper
