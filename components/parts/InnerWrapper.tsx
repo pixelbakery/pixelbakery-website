151bb711b304935ea14ad52cc9d6e0b87f15f63b
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-05 04:53:29 -0600

    fix: fonts not loading in production

--- components/parts/InnerWrapper.tsx
+++ components/parts/InnerWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, PropsWithChildren } from 'react'
+import { ReactNode, PropsWithChildren } from 'react'
 import cn from 'classnames'
 import { backgroundColorVariants, backgroundColorVariant } from '@lib/colorHelper'
 
