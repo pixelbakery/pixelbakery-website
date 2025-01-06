d6a645066f5dfd213f48b956dea56e9b9d0bbf09
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 13:41:39 -0600

    fix: date formatter 2

--- components/pageSpecific/careers/Careers_Card.tsx
+++ components/pageSpecific/careers/Careers_Card.tsx
@@ -1,7 +1,21 @@
-import DateFormatter from '@lib/date-formatter'
+import { dateFormatter } from '@lib'
 import Link from 'next/link'
+interface JobData {
+  title: string
+  commitment: string
+  location: string
+  date: string | Date
+}
 
-function Careers_Card({ job }) {
+interface Job {
+  filePath: string
+  data: JobData
+}
+
+interface CareersCardProps {
+  job: Job
+}
+function Careers_Card({ job }: CareersCardProps) {
   return (
     <Link
       hrefLang={'en-US'}
@@ -9,19 +23,19 @@ function Careers_Card({ job }) {
       href={`/careers/[slug]`}
       className='col-span-1'
     >
-      <article className='h-full w-full px-3 py-4 border-4 border-pink rounded-lg flex flex-col justify-between cursor-pointer duration-300 hover:scale-98'>
-        <div className='text-blue-dark bg-yellow font-bold py-1 px-2 mr-auto rounded-md '>
+      <article className='flex flex-col justify-between w-full h-full px-3 py-4 duration-300 border-4 rounded-lg cursor-pointer border-pink hover:scale-98'>
+        <div className='px-2 py-1 mr-auto font-bold rounded-md text-blue-dark bg-yellow '>
           {job.data.commitment}
         </div>
         <div className='py-8'>
-          <h3 className='text-pink text-3xl mb-0 pb-0 leading-none'>{job.data.title}</h3>
-          <small className='text-blue-dark font-semibold opacity-80 my-0 py-0 leading-none '>
+          <h3 className='pb-0 mb-0 text-3xl leading-none text-pink'>{job.data.title}</h3>
+          <small className='py-0 my-0 font-semibold leading-none text-blue-dark opacity-80 '>
             {job.data.location}
           </small>
         </div>
-        <small className='text-wine opacity-50 text-2xs leading-tight'>
+        <small className='leading-tight opacity-50 text-wine text-2xs'>
           posted on <br />
-          <DateFormatter dateString={job.data.date} />
+          {dateFormatter({ dateString: job.data.date })}
         </small>
       </article>
     </Link>
