7eee28caa1c3f3a7394a38fb64927624ccba5410
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 03:23:09 -0600

    test: debugging vercel font issue again

--- pages/careers/[slug].tsx
+++ pages/careers/[slug].tsx
@@ -1,54 +1,48 @@
-// React imports
-import { ReactElement } from 'react'
+//react imports
+import { ReactElement, useState } from 'react'
 
-// MDX & frontmatter imports
+//MDX & frontmatter imports
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { jobFilePaths, JOBS_PATH } from '@lib/mdxUtils'
-import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import remarkGfm from 'remark-gfm'
 
-// Next.js imports
+//nextJS imports
 import Image from 'next/image'
+import Head from 'next/head'
 import Link from 'next/link'
-import type { GetStaticPaths, GetStaticProps } from 'next/types'
 
-// Component imports
-import { InnerWrapper, PageSection, Button_Filled } from '@parts'
+import { InnerWrapper, PageSection, Button_Filled } from '@parts/index'
+
 import { H1, H2AndLead } from '@typography/index'
+
 import DateFormatter from '@lib/date-formatter'
-import Careers_SEO from '@careers/Careers_Post_SEO'
-import Layout_Default from 'components/layouts/Layout_Default'
 
-// CSS imports
+//css imports
 import markdownStyles from '@styles/markdown-styles.module.css'
-
-// Utility imports
+import Careers_SEO from '@careers/Careers_Post_SEO'
 import dayjs from 'dayjs'
+import Layout_Defaualt from 'components/layouts/Layout_Default'
 
-// Define the structure of the frontmatter and props
-interface FrontMatter {
-  title: string
-  date: string
-  commitment: string
-  coverImage?: string
-  active: boolean
+const components = {
+  Head,
 }
+const ImgPrefix = process.env.NEXT_PUBLIC_IMG_PREFIX
 
-interface PageJobPostingProps {
-  slug: string
-  source: MDXRemoteSerializeResult
-  frontMatter: FrontMatter
-}
-
-// Define the layout for the page
-const Page_JobPosting = ({ slug, source, frontMatter }: PageJobPostingProps): ReactElement => {
-  const coverImage = frontMatter.coverImage || '/img/pixel-bakery-samee-dan-1200x900.png'
+function Page_JobPosting({ slug, source, frontMatter }) {
+  const [coverImage] = useState(handleCoverImage())
+  function handleCoverImage() {
+    if (frontMatter.coverImage != undefined) {
+      return `${frontMatter.coverImage}`
+    } else {
+      return `/img/pixel-bakery-samee-dan-1200x900.png`
+    }
+  }
   const datePostedISO = dayjs(frontMatter.date).toISOString()
   const dateExpiredISO = dayjs(datePostedISO).add(2, 'month').toISOString()
-
   return (
     <>
       <Careers_SEO
@@ -66,7 +60,7 @@ const Page_JobPosting = ({ slug, source, frontMatter }: PageJobPostingProps): Re
             <header className='max-w-2xl mx-auto'>
               <div className='pb-12'>
                 <Link
-                  hrefLang='en-US'
+                  hrefLang={'en-US'}
                   href='/careers'
                   className='pb-1 border-b-2 border-b-blue text-blue'
                 >
@@ -79,51 +73,55 @@ const Page_JobPosting = ({ slug, source, frontMatter }: PageJobPostingProps): Re
               <H1 color='blue' className='text-6xl text-blue'>
                 {frontMatter.title}
               </H1>
-              <div className='mx-auto md:max-w-6xl'>
+              <div className='mx-auto  md:max-w-6xl'>
                 <div className='w-full mx-auto mb-24 aspect-w-4 aspect-h-3 bg-peach'>
                   <Image
                     fill={true}
                     className='object-cover object-center w-full h-full'
-                    src={coverImage}
+                    src={`${ImgPrefix}${coverImage}`}
                     alt={`Pixel Bakery is hiring a ${frontMatter.commitment} ${frontMatter.title}`}
                   />
                 </div>
               </div>
-              {!frontMatter.active && (
-                <p className='text-2xl italic font-bold'>Sorry, this position has been filled.</p>
+              {!frontMatter.active ? (
+                <p className='text-2xl italic font-bold '>Sorry, this position has been filled.</p>
+              ) : (
+                ''
               )}
               <p className='pt-0 mt-0 text-sm italic'>
-                posted on <DateFormatter dateString={frontMatter.date} />
+                posted on <DateFormatter dateString={frontMatter.date} />:
               </p>
             </header>
           </InnerWrapper>
 
           <InnerWrapper>
             <div className='max-w-2xl mx-auto'>
-              <div className={markdownStyles.markdown}>
-                <MDXRemote {...source} />
+              <div className={markdownStyles['markdown']}>
+                <MDXRemote {...source} components={components} />
               </div>
-              {frontMatter.active && (
+              {!frontMatter.active ? (
+                ''
+              ) : (
                 <div className='flex flex-col justify-center mt-32'>
                   <H2AndLead
                     headerColor='blue-dark'
                     leadColor='peach'
-                    headerText='Does that sound like you?'
-                    leadText='great, get at it.'
+                    headerText={'Does that sound like you?'}
+                    leadText={'great, get at it.'}
                   />
                   <Button_Filled
                     text='apply for position'
-                    bgColor='pink'
-                    textColor='pink-lighter'
+                    bgColor={'pink'}
+                    textColor={'pink-lighter'}
                     chevronDirection='right'
-                    link='/careers/application'
+                    link={'/careers/application'}
                     center={false}
                   />
                 </div>
               )}
               <div className='pt-12'>
                 <Link
-                  hrefLang='en-US'
+                  hrefLang={'en-US'}
                   href='/careers'
                   className='pb-1 border-b-2 text-blue border-b-blue'
                 >
@@ -137,38 +135,19 @@ const Page_JobPosting = ({ slug, source, frontMatter }: PageJobPostingProps): Re
     </>
   )
 }
-
-// Set layout for the page
+//Set page layout
 Page_JobPosting.getLayout = function getLayout(page: ReactElement) {
-  return <Layout_Default>{page}</Layout_Default>
+  return <Layout_Defaualt>{page}</Layout_Defaualt>
 }
 
 export default Page_JobPosting
-
-// Static Props
-export const getStaticProps: GetStaticProps<PageJobPostingProps> = async ({ params }) => {
-  const slug = Array.isArray(params?.slug) ? params.slug[0] : params?.slug
-
-  if (!slug) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const jobsFilePath = path.join(JOBS_PATH, `${slug}.mdx`)
-  const source = fs.readFileSync(jobsFilePath, 'utf8')
+export const getStaticProps = async ({ params }) => {
+  //MDX Stuff
+  const jobsFilePath = path.join(JOBS_PATH, `${params.slug}.mdx`.toString())
+  const source = fs.readFileSync(jobsFilePath)
   const { content, data } = matter(source)
-
-  // Validate and ensure all required fields in frontMatter
-  const frontMatter: FrontMatter = {
-    title: data.title || 'Untitled',
-    date: data.date || new Date().toISOString(),
-    commitment: data.commitment || 'Unknown commitment',
-    active: data.active !== undefined ? data.active : true,
-    coverImage: data.coverImage || undefined,
-  }
-
   const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
     mdxOptions: {
       remarkPlugins: [remarkGfm],
       rehypePlugins: [],
@@ -176,20 +155,19 @@ export const getStaticProps: GetStaticProps<PageJobPostingProps> = async ({ para
     },
     scope: data,
   })
-
+  data.date = JSON.parse(JSON.stringify(data.date))
   return {
     props: {
-      slug,
+      slug: params.slug,
       source: mdxSource,
-      frontMatter,
+      frontMatter: data,
     },
   }
 }
 
-// Static Paths
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getStaticPaths = async () => {
   const paths = jobFilePaths
-    .map((filePath) => filePath.replace(/\.mdx?$/, ''))
+    .map((path) => path.replace(/\.mdx?$/, ''))
     .map((slug) => ({ params: { slug } }))
 
   return {
