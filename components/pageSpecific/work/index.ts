d9d09c21527f204c66c604f32c01bf7fead77328
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 13:03:00 -0600

    fix: missing barrel file

---
+++ components/pageSpecific/work/index.ts
@@ -0,0 +1,8 @@
+export { default as Work_Callout } from './Work_Callout'
+export { default as Work_Clients } from './Work_Clients'
+export { default as Work_Industries } from './Work_Industries'
+export { default as Work_Portfolio } from './Work_Portfolio'
+export { default as Work_Portfolio_Card } from './Work_Portfolio_Card'
+export { default as Work_SEO } from './Work_SEO'
+export { default as Work_Services } from './Work_Services'
+export { default as Work_Services_Card } from './Work_Services_Card'
