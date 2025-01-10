b7c7d4a2a74aab964cf06d128516a8d0cd1e728c
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-10 11:51:51 -0600

    refactor: remove unused variables and simplify sendFreelancers-confirmation

--- pages/api/sendFreelancers-confirmation.ts
+++ pages/api/sendFreelancers-confirmation.ts
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 import mail from '@sendgrid/mail'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest } from 'next'
 
 const apiKey = process.env.NEXT_PUBLIC_SENDGRID_API_KEY
 if (!apiKey) {
@@ -9,38 +8,32 @@ if (!apiKey) {
 
 mail.setApiKey(apiKey)
 
-export default async function sendJobApplication(req: NextApiRequest, res: NextApiResponse) {
+export default async function sendJobApplication(req: NextApiRequest) {
   const body = JSON.parse(req.body)
 
-  let skills = ''
-  if (body.skills) {
-    skills = body.skills.map((c: any) => c.value).join(', ')
+  await mail.send({
+    to: `${body.email}`,
+    from: {
+      email: 'careers@pixelbakery.com',
+      name: 'Pixel Bakery Robot',
+    },
 
-    await mail.send({
-      to: `${body.email}`,
-      from: {
-        email: 'careers@pixelbakery.com',
-        name: 'Pixel Bakery Robot',
-      },
-
-      subject: `Freelancer Application: ${body.name}`,
-      templateId: 'd-2f3df831fdd04ac0b9a1baf71c141603',
-      dynamicTemplateData: {
-        about: `${body.about}`,
-        skills: `${skills}`,
-        name: `${body.name}`,
-        title: `${body.title}`,
-        address: `${body.address}`,
-        experience: `${body.experience}`,
-        bird: `${body.bird}`,
-        rate: `${body.rate}`,
-        email: `${body.email}`,
-        referral: `${body.referral}`,
-        phone: `${body.phone}`,
-        pronoun: `${body.pronoun}`,
-        social: `${body.social}`,
-        website: `${body.website}`,
-      },
-    })
-  }
+    subject: `Freelancer Application: ${body.name}`,
+    templateId: 'd-2f3df831fdd04ac0b9a1baf71c141603',
+    dynamicTemplateData: {
+      about: `${body.about}`,
+      name: `${body.name}`,
+      title: `${body.title}`,
+      address: `${body.address}`,
+      experience: `${body.experience}`,
+      bird: `${body.bird}`,
+      rate: `${body.rate}`,
+      email: `${body.email}`,
+      referral: `${body.referral}`,
+      phone: `${body.phone}`,
+      pronoun: `${body.pronoun}`,
+      social: `${body.social}`,
+      website: `${body.website}`,
+    },
+  })
 }
