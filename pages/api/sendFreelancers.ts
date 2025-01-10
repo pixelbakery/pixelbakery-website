e12395f8405d1788e871e68c8a230a7b4445e38a
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-10 11:52:18 -0600

    refactor: no longer using monday api to grab skills

--- pages/api/sendFreelancers.ts
+++ pages/api/sendFreelancers.ts
@@ -17,38 +17,33 @@ export default async function sendJobApplication(req: NextApiRequest, res: NextA
     sendTo = 'hello@pixelbakery.com'
   }
   const body = JSON.parse(req.body)
-  let skills = ''
-  if (body.skills) {
-    skills = body.skills.map((c: { value: any }) => c.value).join(', ')
 
-    await mail.send({
-      to: `${sendTo}`,
-      from: {
-        email: 'careers@pixelbakery.com',
-        name: 'Pixel Bakery Robot',
-      },
-      replyTo: {
-        email: `${body.email}`,
-        name: `${body.name}`,
-      },
-      subject: `Freelancer Application: ${body.name}`,
-      templateId: 'd-6bd8e8e0bc41454f8a28705be9d8cc43',
-      dynamicTemplateData: {
-        about: `${body.about}`,
-        skills: `${skills}`,
-        name: `${body.name}`,
-        title: `${body.title}`,
-        address: `${body.address}`,
-        experience: `${body.experience}`,
-        bird: `${body.bird}`,
-        rate: `${body.rate}`,
-        email: `${body.email}`,
-        referral: `${body.referral}`,
-        phone: `${body.phone}`,
-        pronoun: `${body.pronoun}`,
-        social: `${body.social}`,
-        website: `${body.website}`,
-      },
-    })
-  }
+  await mail.send({
+    to: `${sendTo}`,
+    from: {
+      email: 'careers@pixelbakery.com',
+      name: 'Pixel Bakery Robot',
+    },
+    replyTo: {
+      email: `${body.email}`,
+      name: `${body.name}`,
+    },
+    subject: `Freelancer Application: ${body.name}`,
+    templateId: 'd-6bd8e8e0bc41454f8a28705be9d8cc43',
+    dynamicTemplateData: {
+      about: `${body.about}`,
+      name: `${body.name}`,
+      title: `${body.title}`,
+      address: `${body.address}`,
+      experience: `${body.experience}`,
+      bird: `${body.bird}`,
+      rate: `${body.rate}`,
+      email: `${body.email}`,
+      referral: `${body.referral}`,
+      phone: `${body.phone}`,
+      pronoun: `${body.pronoun}`,
+      social: `${body.social}`,
+      website: `${body.website}`,
+    },
+  })
 }
